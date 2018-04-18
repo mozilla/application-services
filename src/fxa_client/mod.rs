fe750a8b4088a6738b2c1b72435c9e6dce36bb49
Edouard Oger <eoger@fastmail.com>
2018-04-18 11:08:52 -0400

    Swap Hkdf::extract arguments.
    They were in the wrong order after the migration to the newest version of the
    crate.

--- src/fxa_client/mod.rs
+++ src/fxa_client/mod.rs
@@ -134,7 +134,7 @@ impl<'a> FxAClient<'a> {
   }
 
   fn derive_hkdf_sha256_key(ikm: &[u8], xts: &[u8], info: &[u8], len: usize) -> Vec<u8> {
-    let hk = Hkdf::<Sha256>::extract(&ikm, &xts);
+    let hk = Hkdf::<Sha256>::extract(&xts, &ikm);
     hk.expand(&info, len)
   }
 
