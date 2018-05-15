98b5e20575fbdae9d23d51e225218817940ec641
Edouard Oger <eoger@fastmail.com>
2018-05-15 19:49:01 -0400

    Fix CI build

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -3,20 +3,27 @@
 files=(
     "fxa-rust-client"
     "fxa-rust-client/ffi"
-    "rust-cjose"
     "boxlocker"
     "sync15-adapter"
     "sync15-adapter/ffi"
 )
 
+export JANSSON_DIR="$PWD""/libs/ios/jansson/lib"
+export OPENSSL_STATIC=0 OPENSSL_DIR="$PWD""/libs/ios/openssl"
+export CJOSE_DIR="$PWD""/libs/ios/cjose/lib"
 
+pushd .
+cd libs/ios
+./build-all.sh
+popd
 
 for i in "${files[@]}"
 do
    :
    echo "Testing: $i"
+   pushd .
    cd $i
    cargo build --release
    cargo test
-   cd ..
+   popd
 done
