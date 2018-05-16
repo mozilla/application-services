764254594cf5b430a6a9e9fdf92ba287cbf793ef
Vlad Filippov <vlad.filippov@gmail.com>
2018-05-15 23:16:13 -0400

    Reconfigure workspaces, build on Travis (#6)

---
+++ scripts/ci-test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -ex
+
+files=(
+    "fxa-rust-client"
+    "fxa-rust-client/ffi"
+    "boxlocker"
+    "sync15-adapter"
+    "sync15-adapter/ffi"
+)
+
+export JANSSON_DIR="$PWD""/libs/ios/jansson/lib"
+export OPENSSL_STATIC=0 OPENSSL_DIR="$PWD""/libs/ios/openssl"
+export CJOSE_DIR="$PWD""/libs/ios/cjose/lib"
+
+pushd .
+cd libs/ios
+./build-all.sh
+popd
+
+for i in "${files[@]}"
+do
+   :
+   echo "Testing: $i"
+   pushd .
+   cd $i
+   cargo test
+   popd
+done
