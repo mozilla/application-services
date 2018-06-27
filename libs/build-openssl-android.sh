056d41acd947fd4ecf0ffb7ebe6d0fe042df97d7
Edouard Oger <eoger@fastmail.com>
2018-06-27 19:22:06 -0400

    Make openssl libraries writable after build

--- libs/build-openssl-android.sh
+++ libs/build-openssl-android.sh
@@ -54,7 +54,10 @@ make clean || true
 make && make install
 mkdir -p "$DIST_DIR""/include/openssl"
 mkdir -p "$DIST_DIR""/lib"
-cp -p "$OPENSSL_OUTPUT_PATH"/lib/libssl.so "$DIST_DIR""/lib"
-cp -p "$OPENSSL_OUTPUT_PATH"/lib/libcrypto.so "$DIST_DIR""/lib"
+cp -p "$OPENSSL_OUTPUT_PATH"/lib/libssl.so "$DIST_DIR"/lib
+cp -p "$OPENSSL_OUTPUT_PATH"/lib/libcrypto.so "$DIST_DIR"/lib
 cp -L "$PWD"/include/openssl/*.h "${DIST_DIR}/include/openssl"
+# For some reason the created binaries are -w.
+chmod +w "$DIST_DIR"/lib/libssl.so
+chmod +w "$DIST_DIR"/lib/libcrypto.so
 rm -rf "$OPENSSL_OUTPUT_PATH"
