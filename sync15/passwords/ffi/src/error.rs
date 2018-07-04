d4896d61706ba84bcd708d16076be101c15b6228
Nick Alexander <nalexander@mozilla.com>
2018-07-03 21:26:33 -0700

    [sync15/passwords] Initial version of sync15/passwords on top of `logins` and Mentat.

---
+++ sync15/passwords/ffi/src/error.rs
@@ -0,0 +1,126 @@
+// Copyright 2018 Mozilla
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+use ffi_toolkit::string::{
+    string_to_c_char
+};
+use std::ptr;
+use std::os::raw::c_char;
+
+use sync15_passwords::{
+    Result,
+};
+
+pub unsafe fn with_translated_result<F, T>(error: *mut ExternError, callback: F) -> *mut T
+where F: FnOnce() -> Result<T> {
+    translate_result(callback(), error)
+}
+
+pub unsafe fn with_translated_void_result<F>(error: *mut ExternError, callback: F)
+where F: FnOnce() -> Result<()> {
+    translate_void_result(callback(), error);
+}
+
+pub unsafe fn with_translated_string_result<F>(error: *mut ExternError, callback: F) -> *mut c_char
+where F: FnOnce() -> Result<String> {
+    if let Some(s) = try_translate_result(callback(), error) {
+        string_to_c_char(s)
+    } else {
+        ptr::null_mut()
+    }
+}
+
+pub unsafe fn with_translated_opt_string_result<F>(error: *mut ExternError, callback: F) -> *mut c_char
+where F: FnOnce() -> Result<Option<String>> {
+    if let Some(Some(s)) = try_translate_result(callback(), error) {
+        string_to_c_char(s)
+    } else {
+        // This is either an error case, or callback returned None.
+        ptr::null_mut()
+    }
+}
+
+// XXX rest of this is COPYPASTE from mentat/ffi/util.rs, this likely belongs in ffi-toolkit
+// (something similar is there, but it is more error prone and not usable in a general way)
+// XXX Actually, once errors are more stable we should do something more like fxa (and put that in
+// ffi-toolkit). Yesterday I thought this was impossible but IDK I was tired? It's possible.
+
+/// Represents an error that occurred on the mentat side. Many mentat FFI functions take a
+/// `*mut ExternError` as the last argument. This is an out parameter that indicates an
+/// error that occurred during that function's execution (if any).
+///
+/// For functions that use this pattern, if the ExternError's message property is null, then no
+/// error occurred. If the message is non-null then it contains a string description of the
+/// error that occurred.
+///
+/// Important: This message is allocated on the heap and it is the consumer's responsibility to
+/// free it using `destroy_mentat_string`!
+///
+/// While this pattern is not ergonomic in Rust, it offers two main benefits:
+///
+/// 1. It avoids defining a large number of `Result`-shaped types in the FFI consumer, as would
+///    be required with something like an `struct ExternResult<T> { ok: *mut T, err:... }`
+/// 2. It offers additional type safety over `struct ExternResult { ok: *mut c_void, err:... }`,
+///    which helps avoid memory safety errors.
+#[repr(C)]
+#[derive(Debug)]
+pub struct ExternError {
+    pub message: *mut c_char,
+    // TODO: Include an error code here.
+}
+
+impl Default for ExternError {
+    fn default() -> ExternError {
+        ExternError { message: ptr::null_mut() }
+    }
+}
+/// Translate Result<T, E>, into something C can understand, when T is not `#[repr(C)]`
+///
+/// - If `result` is `Ok(v)`, moves `v` to the heap and returns a pointer to it, and sets
+///   `error` to a state indicating that no error occurred (`message` is null).
+/// - If `result` is `Err(e)`, returns a null pointer and stores a string representing the error
+///   message (which was allocated on the heap and should eventually be freed) into
+///   `error.message`
+pub unsafe fn translate_result<T>(result: Result<T>, error: *mut ExternError) -> *mut T {
+    // TODO: can't unwind across FFI...
+    assert!(!error.is_null(), "Error output parameter is not optional");
+    let error = &mut *error;
+    error.message = ptr::null_mut();
+    match result {
+        Ok(val) => Box::into_raw(Box::new(val)),
+        Err(e) => {
+            error.message = string_to_c_char(e.to_string());
+            ptr::null_mut()
+        }
+    }
+}
+
+pub unsafe fn try_translate_result<T>(result: Result<T>, error: *mut ExternError) -> Option<T> {
+    // TODO: can't unwind across FFI...
+    assert!(!error.is_null(), "Error output parameter is not optional");
+    let error = &mut *error;
+    error.message = ptr::null_mut();
+    match result {
+        Ok(val) => Some(val),
+        Err(e) => {
+            error.message = string_to_c_char(e.to_string());
+            None
+        }
+    }
+}
+
+/// Identical to `translate_result`, but with additional type checking for the case that we have
+/// a `Result<(), E>` (which we're about to drop on the floor).
+pub unsafe fn translate_void_result(result: Result<()>, error: *mut ExternError) {
+    // TODO: update this comment.
+    // Note that Box<T> guarantees that if T is zero sized, it's not heap allocated. So not
+    // only do we never need to free the return value of this, it would be a problem if someone did.
+    translate_result(result, error);
+}
---
+++ sync15/passwords/src/engine.rs
@@ -0,0 +1,230 @@
+// Copyright 2018 Mozilla
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+use sync15_adapter as sync;
+use self::sync::{
+    ServerTimestamp,
+    OutgoingChangeset,
+    Payload,
+};
+
+use mentat::{
+    self,
+    DateTime,
+    FromMillis,
+    Utc,
+};
+
+use logins::{
+    credentials,
+    passwords,
+    Credential,
+    CredentialId,
+    SyncGuid,
+    ServerPassword,
+    ensure_vocabulary,
+};
+
+use std::collections::BTreeSet;
+
+use errors::{
+    Sync15PasswordsError,
+    Result,
+};
+
+// TODO: These probably don't all need to be public!
+pub struct PasswordEngine {
+    pub last_server_timestamp: ServerTimestamp,
+    pub current_tx_id: Option<mentat::Entid>,
+    pub store: mentat::Store,
+}
+
+impl PasswordEngine {
+
+    pub fn new(mut store: mentat::Store) -> Result<PasswordEngine> {
+        let last_server_timestamp: ServerTimestamp = { // Scope borrow of `store`.
+            let mut in_progress = store.begin_transaction()?;
+
+            ensure_vocabulary(&mut in_progress)?;
+
+            let timestamp = passwords::get_last_server_timestamp(&in_progress)?;
+
+            in_progress.commit()?;
+
+            ServerTimestamp(timestamp.unwrap_or_default())
+        };
+
+        Ok(PasswordEngine {
+            current_tx_id: None,
+            last_server_timestamp,
+            store,
+        })
+    }
+
+    pub fn touch_credential(&mut self, id: String) -> Result<()> {
+        let mut in_progress = self.store.begin_transaction()?;
+        credentials::touch_by_id(&mut in_progress, CredentialId(id), None)?;
+        in_progress.commit()?;
+        Ok(())
+    }
+
+    pub fn delete_credential(&mut self, id: String) -> Result<()> {
+        let mut in_progress = self.store.begin_transaction()?;
+        credentials::delete_by_id(&mut in_progress, CredentialId(id))?;
+        in_progress.commit()?;
+        Ok(())
+    }
+
+    pub fn update_credential(&mut self, id: &str, updater: impl FnMut(&mut Credential)) -> Result<bool> {
+        let mut in_progress = self.store.begin_transaction()?;
+
+        let mut credential = credentials::get_credential(&in_progress, CredentialId(id.into()))?;
+        if credential.as_mut().map(updater).is_none() {
+            return Ok(false);
+        }
+
+        credentials::add_credential(&mut in_progress, credential.unwrap())?;
+        in_progress.commit()?;
+        Ok(true)
+    }
+
+    pub fn sync(&mut self, svc: &sync::Sync15Service) -> Result<()> {
+        let ts = self.last_server_timestamp;
+        sync::synchronize(svc, self, "passwords".into(), ts, true)?;
+        Ok(())
+    }
+
+    pub fn reset(&mut self) -> Result<()> {
+        { // Scope borrow of self.
+            let mut in_progress = self.store.begin_transaction()?;
+            passwords::reset_client(&mut in_progress)?;
+            in_progress.commit()?;
+        }
+
+        self.last_server_timestamp = 0.0.into();
+
+        Ok(())
+    }
+
+    pub fn wipe(&mut self) -> Result<()> {
+        self.last_server_timestamp = 0.0.into();
+
+        // let mut in_progress = store.begin_transaction().map_err(|_| "failed to begin_transaction")?;
+        // // reset_client(&mut in_progress).map_err(|_| "failed to reset_client")?;
+        // in_progress.commit().map_err(|_| "failed to commit")?;
+
+        // self.save()?;
+        Ok(())
+    }
+
+    pub fn get_unsynced_changes(&mut self) -> Result<(Vec<Payload>, ServerTimestamp)> {
+        let mut result = vec![];
+
+        let in_progress_read = self.store.begin_read()?;
+
+        let deleted = passwords::get_deleted_sync_password_uuids_to_upload(&in_progress_read)?;
+        println!("{} deleted records to upload: {:?}", deleted.len(), deleted);
+
+        for r in deleted {
+            result.push(Payload::new_tombstone(r.0))
+        }
+
+        let modified = passwords::get_modified_sync_passwords_to_upload(&in_progress_read)?;
+        println!("{} modified records to upload: {:?}", modified.len(), modified.iter().map(|r| &r.uuid.0).collect::<Vec<_>>());
+
+        for r in modified {
+            result.push(Payload::from_record(r)?);
+        }
+
+        Ok((result, self.last_server_timestamp))
+    }
+}
+
+impl sync::Store for PasswordEngine {
+    type Error = Sync15PasswordsError;
+
+    fn apply_incoming(
+        &mut self,
+        inbound: sync::IncomingChangeset
+    ) -> Result<OutgoingChangeset> {
+        println!("Remote collection has {} changes timestamped at {}",
+               inbound.changes.len(), inbound.timestamp);
+
+        { // Scope borrow of inbound.changes.
+            let (to_delete, to_apply): (Vec<_>, Vec<_>) = inbound.changes.iter().partition(|(payload, _)| payload.is_tombstone());
+            println!("{} records to delete: {:?}", to_delete.len(), to_delete);
+            println!("{} records to apply: {:?}", to_apply.len(), to_apply);
+        }
+
+        self.current_tx_id = { // Scope borrow of self.
+            let mut in_progress = self.store.begin_transaction()?;
+
+            for (payload, server_timestamp) in inbound.changes {
+                if payload.is_tombstone() {
+                    passwords::delete_by_sync_uuid(&mut in_progress, payload.id().into())?;
+                } else {
+                    println!("Applying: {:?}", payload);
+
+                    let mut server_password: ServerPassword = payload.clone().into_record()?;
+                    server_password.modified = DateTime::<Utc>::from_millis(server_timestamp.as_millis() as i64);
+
+                    passwords::apply_password(&mut in_progress, server_password)?;
+                }
+            }
+
+            let current_tx_id = in_progress.last_tx_id();
+            in_progress.commit()?;
+
+            Some(current_tx_id)
+        };
+
+        let (outbound_changes, last_server_timestamp) = self.get_unsynced_changes()?;
+
+        let outbound = OutgoingChangeset {
+            changes: outbound_changes,
+            timestamp: last_server_timestamp,
+            collection: "passwords".into()
+        };
+
+        println!("After applying incoming changes, local collection has {} outgoing changes timestamped at {}",
+               outbound.changes.len(), outbound.timestamp);
+
+        Ok(outbound)
+    }
+
+    fn sync_finished(&mut self, new_last_server_timestamp: ServerTimestamp, records_synced: &[String]) -> Result<()> {
+        println!("Synced {} outbound changes at remote timestamp {}", records_synced.len(), new_last_server_timestamp);
+        for id in records_synced {
+            println!("  {:?}", id);
+        }
+
+        let current_tx_id = self.current_tx_id.unwrap(); // XXX
+
+        { // Scope borrow of self.
+            let mut in_progress = self.store.begin_transaction()?;
+
+            let deleted = passwords::get_deleted_sync_password_uuids_to_upload(&in_progress)?;
+            let deleted: BTreeSet<String> = deleted.into_iter().map(|x| x.0).collect();
+
+            let (deleted, uploaded): (Vec<_>, Vec<_>) =
+                records_synced.iter().cloned().partition(|id| deleted.contains(id));
+
+            passwords::mark_synced_by_sync_uuids(&mut in_progress, uploaded.into_iter().map(SyncGuid).collect(), current_tx_id)?;
+            passwords::delete_by_sync_uuids(&mut in_progress, deleted.into_iter().map(SyncGuid).collect())?;
+
+            passwords::set_last_server_timestamp(&mut in_progress, new_last_server_timestamp.0)?;
+
+            in_progress.commit()?;
+        };
+
+        self.last_server_timestamp = new_last_server_timestamp;
+        Ok(())
+    }
+}
