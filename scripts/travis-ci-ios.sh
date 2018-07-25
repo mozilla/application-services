e4972fd76cfad2e3f552edb6297ab65d891082b2
Edouard Oger <eoger@fastmail.com>
2018-07-25 14:10:34 -0400

    Travis FxA iOS builds

---
+++ scripts/travis-ci-ios.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+rustup target add aarch64-apple-ios
+rustup target add armv7-apple-ios
+rustup target add i386-apple-ios
+rustup target add x86_64-apple-ios
+cargo install --git https://github.com/TimNN/cargo-lipo
+cd fxa-rust-client/sdks
+carthage build --no-skip-current --verbose && carthage archive
+rm -rf Carthage
+mv FxAClient.framework.zip ../..
