40674f9a65cb962005a98d5b83ad3b02c635e79e
Edouard Oger <eoger@fastmail.com>
2018-08-14 01:41:40 -0400

    Disable travis cache (#183)
    * Force cargo lipo install
    
    * Disable cargo caching

--- scripts/travis-ci-ios.sh
+++ scripts/travis-ci-ios.sh
@@ -6,7 +6,7 @@ rustup target add aarch64-apple-ios
 rustup target add armv7-apple-ios
 rustup target add i386-apple-ios
 rustup target add x86_64-apple-ios
-cargo install --git https://github.com/TimNN/cargo-lipo
+cargo install --force --git https://github.com/TimNN/cargo-lipo
 cd fxa-client/sdks
 carthage build --no-skip-current --verbose && carthage archive
 rm -rf Carthage
