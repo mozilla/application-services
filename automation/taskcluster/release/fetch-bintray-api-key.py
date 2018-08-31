e3e979a9fe9c832fa4bc099657d501fc0031a3d4
Nick Alexander <nalexander@mozilla.com>
2018-08-31 14:17:52 -0700

    Publish logins AAR to nalexander's personal bintray.

--- automation/taskcluster/release/fetch-bintray-api-key.py
+++ automation/taskcluster/release/fetch-bintray-api-key.py
@@ -5,7 +5,7 @@
 import os
 import taskcluster
 
-SECRET_NAME = 'project/mentat/publish'
+SECRET_NAME = 'project/application-services/publish'
 TASKCLUSTER_BASE_URL = 'http://taskcluster/secrets/v1'
 
 
