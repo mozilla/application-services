988104f1c57614a7e348e2ff4b2de617dee43dc2
Thom Chiovoloni <tchiovoloni@mozilla.com>
2018-09-19 23:16:19 -0700

    Rough version of swift API to logins, should it turn out it's needed. Probably has serious flaws.

---
+++ logins-swift/Sync15Passwords/Sync15Logins/Sync15Logins.h
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sync15Logins.
+FOUNDATION_EXPORT double Sync15LoginsVersionNumber;
+
+//! Project version string for Sync15Logins.
+FOUNDATION_EXPORT const unsigned char Sync15LoginsVersionString[];
+
+#import <Sync15Logins/RustPasswordAPI.h>
