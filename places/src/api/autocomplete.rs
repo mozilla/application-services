d052460cffbda7ebb9bfc54aca83b54f5431dd9c
Thom Chiovoloni <tchiovoloni@mozilla.com>
2018-09-24 14:02:52 -0700

    Do enough of autocomplete as to unstick myself

--- places/src/api/autocomplete.rs
+++ places/src/api/autocomplete.rs
@@ -9,43 +9,60 @@ use error::*;
 use super::connection::{Connection};
 
 // rusqlite imports probably reflect that some of this should be in ::storage
-use rusqlite::{Row};
+use rusqlite;
 
 #[derive(Debug)]
 pub struct SearchParams {
     search_string: String,
-}
-
-impl SearchParams {
-    // Can't work out how to make this work :/
-    /*
-    fn named_params(&self) -> &[(&str, &ToSql)] {
-        &[(":searchString", &self.search_string)]
-    }
-    */
+    limit: u32
 }
 
 #[derive(Debug)]
 pub struct SearchResult {
+    title: Option<String>,
     url: Url,
 }
 
 impl SearchResult {
-    pub fn from_row(row: &Row) -> Result<Self> {
+    pub fn from_row(row: &rusqlite::Row) -> Result<Self> {
         Ok(Self {
+            title: row.get_checked::<_, Option<String>>("title")?,
             url: Url::parse(&row.get_checked::<_, Option<String>>("url")?.expect("we gotta have a url in the resultset"))?,
         })
     }
 }
 
-pub fn search_frecent(conn: &Connection, params: SearchParams) /* -> iter of SearchResult */ -> Result<Option<SearchResult>> {
+// There's a lot of boilerplate to return an iterator here (we can't just return
+// `impl Iterator<Item = ...> + 'a` because of rusqlite restrictions)
+pub fn search_frecent(conn: &Connection, params: SearchParams) -> Result<Vec<SearchResult>> {
     // * result should be an iter of SearchResult...
     // * should have a "::storage" layer and not touch the db directly?
     // * obvs the most trivial impl possible!
     // * etc...
-    let sql = "SELECT url from moz_places where url like :searchString";
-    // let params = params.named_params();
-    Ok(conn.get_db().query_row_named(sql, &[(":searchString", &params.search_string)], SearchResult::from_row)?)
+
+    let sql = "
+        SELECT url, title
+        FROM moz_places
+        WHERE url LIKE :search_string
+          OR title LIKE :search_string
+        LIMIT :max
+    ";
+    // Note: I've moved these here since they refer to strings in the sql, so it seems like
+    // we want them to be near the sql and not in a method on SearchResult.
+    let params: &[(&str, &dyn rusqlite::types::ToSql)] = &[
+        (":search_string", &params.search_string),
+        (":max", &params.limit),
+    ];
+    let conn_db = conn.get_db();
+    let mut stmt = conn_db.db.prepare(sql)?;
+    // Couldn't make this work as a collect (probably because of all the Result<>)
+    let mut result = vec![];
+    for res_row in stmt.query_map_named(params, SearchResult::from_row)? {
+        // First ? is rusqlite errors, second is errors in SearchResult::from_row.
+        let parsed = res_row??;
+        result.push(parsed);
+    }
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -69,8 +86,8 @@ mod tests {
         insert(&c, a).expect("should insert");
 
         // phew - finally we can search
-        let maybe = search_frecent(&c, SearchParams { search_string: "%exam%".into() }).expect("must have worked");
-        let result = maybe.expect("should have actually matched too!");
-        assert_eq!(result.url.into_string(), "http://example.com/"); // hrmph - note trailing slash
+        let maybe = search_frecent(&c, SearchParams { search_string: "%exam%".into(), limit: 2 }).expect("must have worked");
+        let result = maybe.get(0).expect("should have actually matched too!");
+        assert_eq!(result.url.to_string(), "http://example.com/"); // hrmph - note trailing slash
     }
 }
