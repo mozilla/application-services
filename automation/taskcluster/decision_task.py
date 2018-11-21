3de10d7ab0a4e632403fb97c6a235a115c989cc3
Nick Alexander <nalexander@mozilla.com>
2018-11-21 11:02:20 -0800

    json.

--- automation/taskcluster/decision_task.py
+++ automation/taskcluster/decision_task.py
@@ -4,9 +4,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import json
 import os.path
 from decisionlib import *
-
 
 
 def populate_chain_of_trust_required_but_unused_files():
