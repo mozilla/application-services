7b8eb59c14e9b6b11ca1c32931de5796a3bf2642
Nick Alexander <nalexander@mozilla.com>
2018-12-14 10:08:01 -0800

    Rename "composites" to "megazords". Fixes #471.

--- megazords/lockbox/android/src/main/java/mozilla/appservices/LockboxMegazord.java
+++ megazords/lockbox/android/src/main/java/mozilla/appservices/LockboxMegazord.java
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package mozilla.appservices.composites;
+package mozilla.appservices;
 
-public class LockboxComposition {
+public class LockboxMegazord {
     public static void init() {
         System.setProperty("mozilla.appservices.fxaclient_ffi_lib_name", "lockbox");
         System.setProperty("mozilla.appservices.logins_ffi_lib_name", "lockbox");
--- megazords/reference-browser/android/src/main/java/mozilla/appservices/ReferenceBrowserMegazord.java
+++ megazords/reference-browser/android/src/main/java/mozilla/appservices/ReferenceBrowserMegazord.java
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package mozilla.appservices.composites;
+package mozilla.appservices;
 
-public class ReferenceBrowserComposition {
+public class ReferenceBrowserMegazord {
     public static void init() {
         System.setProperty("mozilla.appservices.fxaclient_ffi_lib_name", "reference_browser");
         System.setProperty("mozilla.appservices.logins_ffi_lib_name", "reference_browser");
