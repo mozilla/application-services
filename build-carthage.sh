b70ca5f19c288db8608631112efcbbdc2cf6f6d0
Edouard Oger <eoger@fastmail.com>
2019-02-12 18:00:40 -0500

    Add verbose logging

--- build-carthage.sh
+++ build-carthage.sh
@@ -1,5 +1,5 @@
 FRAMEWORK_NAME="${FRAMEWORK_NAME:-MozillaAppServices-frameworks.zip}"
 ## When https://github.com/Carthage/Carthage/issues/2623 is fixed, 
 ## carthage build --archive should work to produce a zip 
-carthage build --no-skip-current && zip -r $FRAMEWORK_NAME Carthage/Build/iOS
+carthage build --no-skip-current --verbose && zip -r $FRAMEWORK_NAME Carthage/Build/iOS
 
