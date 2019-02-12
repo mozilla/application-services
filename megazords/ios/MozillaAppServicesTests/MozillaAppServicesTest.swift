8d442233547b27fcf9b3b679e1b62a793c6761d2
Garvan Keeley <garvankeeley@gmail.com>
2019-02-11 20:08:54 -0500

    Rename and build as static framework Add a readme

--- megazords/ios/MozillaAppServicesTests/MozillaAppServicesTest.swift
+++ megazords/ios/MozillaAppServicesTests/MozillaAppServicesTest.swift
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 import XCTest
 
-@testable import RustAppServices
+@testable import MozillaAppServices
 
 class LoginsTests: XCTestCase {
     func getTestStorage() -> LoginsStorage {
