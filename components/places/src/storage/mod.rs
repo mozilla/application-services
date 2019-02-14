776fe3290953fc5b08cba2f5325eb59fb1527458
Thom Chiovoloni <tchiovoloni@mozilla.com>
2019-02-14 15:56:49 -0800

    Add delete_everything to places using a high water mark

--- components/places/src/storage/mod.rs
+++ components/places/src/storage/mod.rs
@@ -12,7 +12,7 @@ use crate::error::Result;
 use crate::types::{SyncGuid, SyncStatus, Timestamp, VisitTransition};
 use rusqlite::types::{FromSql, FromSqlResult, ToSql, ToSqlOutput, ValueRef};
 use rusqlite::Result as RusqliteResult;
-use rusqlite::Row;
+use rusqlite::{Connection, Row};
 use serde_derive::*;
 use sql_support::{self, ConnExt};
 use std::fmt;
@@ -190,4 +190,21 @@ impl HistoryVisitInfo {
 pub fn run_maintenance(conn: &impl ConnExt) -> Result<()> {
     conn.execute_all(&["VACUUM", "PRAGMA optimize"])?;
     Ok(())
+}
+
+pub(crate) fn put_meta(db: &Connection, key: &str, value: &ToSql) -> Result<()> {
+    db.execute_named_cached(
+        "REPLACE INTO moz_meta (key, value) VALUES (:key, :value)",
+        &[(":key", &key), (":value", value)],
+    )?;
+    Ok(())
+}
+
+pub(crate) fn get_meta<T: FromSql>(db: &Connection, key: &str) -> Result<Option<T>> {
+    let res = db.try_query_one(
+        "SELECT value FROM moz_meta WHERE key = :key",
+        &[(":key", &key)],
+        true,
+    )?;
+    Ok(res)
 }
