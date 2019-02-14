a88d32f2b5834b7080609c3e88dd86e4326e25fc
Mark Hammond <mhammond@skippinet.com.au>
2019-02-14 16:37:10 +1100

    Remove trailing c++ comment delims from .sql files

--- components/places/sql/create_triggers.sql
+++ components/places/sql/create_triggers.sql
@@ -1,6 +1,6 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
--- file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 CREATE TEMP TRIGGER moz_places_afterinsert_trigger
 AFTER INSERT ON moz_places FOR EACH ROW
