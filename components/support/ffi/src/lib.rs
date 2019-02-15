8ba2404d84e7a490a5752074955dd0f7fb071d78
Thom Chiovoloni <tchiovoloni@mozilla.com>
2019-02-15 11:58:57 -0800

    Add #![deny(missing_docs)] to ffi_support

--- components/support/ffi/src/lib.rs
+++ components/support/ffi/src/lib.rs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#![deny(missing_docs)]
+
 //! # FFI Support
 //!
 //! This crate implements a support library to simplify implementing the patterns that the
