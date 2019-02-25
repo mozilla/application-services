9f644e365f364ceb5c0640fea7e5160ea36da099
Edouard Oger <eoger@fastmail.com>
2019-02-25 16:55:11 -0500

    Remove browserid features from FFI

--- components/fxa-client/ffi/src/lib.rs
+++ components/fxa-client/ffi/src/lib.rs
@@ -2,14 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#[cfg(feature = "browserid")]
-use ffi_support::define_box_destructor;
 use ffi_support::{
     define_bytebuffer_destructor, define_handle_map_deleter, define_string_destructor,
     rust_str_from_c, ByteBuffer, ConcurrentHandleMap, ExternError,
 };
-#[cfg(feature = "browserid")]
-use fxa_client::ffi::SyncKeysC;
 use fxa_client::FirefoxAccount;
 use std::os::raw::c_char;
 
@@ -29,36 +25,6 @@ pub extern "C" fn fxa_enable_logcat_logging() {
 
 lazy_static::lazy_static! {
     static ref ACCOUNTS: ConcurrentHandleMap<FirefoxAccount> = ConcurrentHandleMap::new();
-}
-
-/// Creates a [FirefoxAccount] from credentials obtained with the onepw FxA login flow.
-///
-/// This is typically used by the legacy Sync clients: new clients mainly use OAuth flows and
-/// therefore should use `fxa_new`.
-///
-/// # Safety
-///
-/// A destructor [fxa_free] is provided for releasing the memory for this
-/// pointer type.
-#[cfg(feature = "browserid")]
-#[no_mangle]
-pub unsafe extern "C" fn fxa_from_credentials(
-    content_url: *const c_char,
-    client_id: *const c_char,
-    redirect_uri: *const c_char,
-    json: *const c_char,
-    err: &mut ExternError,
-) -> u64 {
-    use fxa_client::WebChannelResponse;
-    log::debug!("fxa_from_credentials");
-    ACCOUNTS.insert_with_result(err, || {
-        let content_url = rust_str_from_c(content_url);
-        let client_id = rust_str_from_c(client_id);
-        let redirect_uri = rust_str_from_c(redirect_uri);
-        let json = rust_str_from_c(json);
-        let resp = WebChannelResponse::from_json(json)?;
-        FirefoxAccount::from_credentials(content_url, client_id, redirect_uri, resp)
-    })
 }
 
 /// Creates a [FirefoxAccount].
@@ -163,41 +129,6 @@ pub extern "C" fn fxa_get_connection_success_url(
     })
 }
 
-/// Generate an assertion for a specified audience. Requires to be in a `Married` state.
-/// Note that new clients don't use assertions and use Oauth flows instead.
-///
-/// # Safety
-///
-/// A destructor [fxa_str_free] is provided for releasing the memory for this
-/// pointer type.
-#[cfg(feature = "browserid")]
-#[no_mangle]
-pub unsafe extern "C" fn fxa_assertion_new(
-    handle: u64,
-    audience: *const c_char,
-    error: &mut ExternError,
-) -> *mut c_char {
-    log::debug!("fxa_assertion_new");
-    ACCOUNTS.call_with_result_mut(error, handle, |fxa| {
-        let audience = rust_str_from_c(audience);
-        fxa.generate_assertion(audience)
-    })
-}
-
-/// Gets the Sync Keys. Requires to be in a `Married` state.
-/// Note that new clients get sync gets using an OAuth flow.
-///
-/// # Safety
-///
-/// A destructor [fxa_sync_keys_free] is provided for releasing the memory for this
-/// pointer type.
-#[cfg(feature = "browserid")]
-#[no_mangle]
-pub extern "C" fn fxa_get_sync_keys(handle: u64, error: &mut ExternError) -> *mut SyncKeysC {
-    log::debug!("fxa_get_sync_keys");
-    ACCOUNTS.call_with_result_mut(error, handle, |fxa| fxa.get_sync_keys())
-}
-
 /// Request a OAuth token by starting a new pairing flow, by calling the content server pairing endpoint.
 ///
 /// This function returns a URL string that the caller should open in a webview.
@@ -293,9 +224,6 @@ pub unsafe extern "C" fn fxa_get_access_token(
     })
 }
 
-define_string_destructor!(fxa_str_free);
-
 define_handle_map_deleter!(ACCOUNTS, fxa_free);
-#[cfg(feature = "browserid")]
-define_box_destructor!(SyncKeysC, fxa_sync_keys_free);
+define_string_destructor!(fxa_str_free);
 define_bytebuffer_destructor!(fxa_bytebuffer_free);
