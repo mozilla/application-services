5cfa9d85f2584170a2a947f6dc08f13a4c7f5870
Edouard Oger <eoger@fastmail.com>
2019-04-17 17:58:47 -0400

    Add top-level comment for rc_crypto

--- components/support/rc_crypto/src/lib.rs
+++ components/support/rc_crypto/src/lib.rs
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/// This crate provides all the cryptographic primitives required by
+/// this workspace, backed by the NSS library.
+/// The exposed API is pretty much the same as the `ring` crate
+/// (https://briansmith.org/rustdoc/ring/) as it is well thought.
 pub mod constant_time;
 pub mod digest;
 mod error;
