f283b890b78a263283e11ca388030280addf692e
Thom Chiovoloni <tchiovoloni@mozilla.com>
2019-04-18 10:38:08 -0700

    Rustup update in the build-carthage.sh script to save iOS devs some pain

--- build-carthage.sh
+++ build-carthage.sh
@@ -3,6 +3,11 @@
 set -euvx
 
 FRAMEWORK_NAME="${1:-MozillaAppServices.framework.zip}"
+
+# Help out iOS folks who might want to run this but haven't
+# updated rust recently.
+rustup update stable
+
 carthage update --platform iOS --cache-builds swift-protobuf
 ## When https://github.com/Carthage/Carthage/issues/2623 is fixed,
 ## carthage build --archive should work to produce a zip
