cd98a4a05888e0abf52dc9fd83362179e3c560d4
Thom Chiovoloni <tchiovoloni@mozilla.com>
2019-05-01 02:05:12 -0700

    Optimize ffi_support::abort_on_panic

--- components/support/ffi/src/lib.rs
+++ components/support/ffi/src/lib.rs
@@ -180,7 +180,7 @@ where
     E: Into<ExternError> + std::fmt::Debug,
     R: IntoFfi,
 {
-    call_with_result_impl(out_error, callback, false)
+    call_with_result_impl(out_error, callback)
 }
 
 /// Call a callback that returns a `T` while:
@@ -205,11 +205,7 @@ where
     call_with_result(out_error, || -> Result<_, ExternError> { Ok(callback()) })
 }
 
-fn call_with_result_impl<R, E, F>(
-    out_error: &mut ExternError,
-    callback: F,
-    abort_on_panic: bool,
-) -> R::Value
+fn call_with_result_impl<R, E, F>(out_error: &mut ExternError, callback: F) -> R::Value
 where
     F: panic::UnwindSafe + FnOnce() -> Result<R, E>,
     E: Into<ExternError> + std::fmt::Debug,
@@ -230,9 +226,6 @@ where
         }
         Err(e) => {
             log::error!("Caught a panic calling rust code: {:?}", e);
-            if abort_on_panic {
-                std::process::abort();
-            }
             *out_error = e.into();
             R::ffi_default()
         }
@@ -243,17 +236,50 @@ where
 /// that aborts, instead of unwinding, on panic.
 pub mod abort_on_panic {
     use super::*;
-    use std::panic::AssertUnwindSafe;
+
+    // Struct that exists to automatically process::abort if we don't call
+    // `std::mem::forget()` on it. This can have substantial performance
+    // benefits over calling `std::panic::catch_unwind` and aborting if a panic
+    // was caught, in addition to not requiring AssertUnwindSafe (for example).
+    struct AbortOnDrop;
+    impl Drop for AbortOnDrop {
+        fn drop(&mut self) {
+            std::process::abort();
+        }
+    }
+
+    /// A helper function useful for cases where you'd like to abort on panic,
+    /// but aren't in a position where you'd like to return an FFI-compatible
+    /// type.
+    #[inline]
+    pub fn with_abort_on_panic<R, F>(callback: F) -> R
+    where
+        F: FnOnce() -> R,
+    {
+        let aborter = AbortOnDrop;
+        let res = callback();
+        std::mem::forget(aborter);
+        res
+    }
 
     /// Same as the root `call_with_result`, but aborts on panic instead of unwinding. See the
     /// `call_with_result` documentation for more.
     pub fn call_with_result<R, E, F>(out_error: &mut ExternError, callback: F) -> R::Value
     where
         F: FnOnce() -> Result<R, E>,
-        E: Into<ExternError> + std::fmt::Debug,
+        E: Into<ExternError>,
         R: IntoFfi,
     {
-        super::call_with_result_impl(out_error, AssertUnwindSafe(callback), true)
+        with_abort_on_panic(|| match callback() {
+            Ok(v) => {
+                *out_error = ExternError::default();
+                v.into_ffi_value()
+            }
+            Err(e) => {
+                *out_error = e.into();
+                R::ffi_default()
+            }
+        })
     }
 
     /// Same as the root `call_with_output`, but aborts on panic instead of unwinding. As a result,
@@ -264,12 +290,7 @@ pub mod abort_on_panic {
         F: FnOnce() -> R,
         R: IntoFfi,
     {
-        let mut dummy = ExternError::success();
-        super::call_with_result_impl(
-            &mut dummy,
-            AssertUnwindSafe(|| -> Result<_, ExternError> { Ok(callback()) }),
-            true,
-        )
+        with_abort_on_panic(callback).into_ffi_value()
     }
 }
 
