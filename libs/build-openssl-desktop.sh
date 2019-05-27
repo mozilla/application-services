cb4468ab297e064601de833652d7aeaa11ea6e17
Edouard Oger <eoger@fastmail.com>
2019-05-27 11:04:11 -0400

    Use the cross-compile tools hosted on tooltool (#1202)

--- libs/build-openssl-desktop.sh
+++ libs/build-openssl-desktop.sh
@@ -55,14 +55,14 @@ if [[ "${CROSS_COMPILE_TARGET}" =~ "darwin" ]]; then
   export CC=/tmp/clang/bin/clang
 
   export TOOLCHAIN_PREFIX=/tmp/cctools/bin
-  export AR=/tmp/cctools/bin/x86_64-apple-darwin11-ar
-  export RANLIB=/tmp/cctools/bin/x86_64-apple-darwin11-ranlib
+  export AR=/tmp/cctools/bin/x86_64-darwin11-ar
+  export RANLIB=/tmp/cctools/bin/x86_64-darwin11-ranlib
 
   LD_LIBRARY_PATH=/tmp/clang/lib ./Configure darwin64-x86_64-cc \
     no-asm shared \
     -march=x86-64 \
     '-B /tmp/cctools/bin' \
-    '-target x86_64-apple-darwin11' \
+    '-target x86_64-darwin11' \
     '-isysroot /tmp/MacOSX10.11.sdk' \
     '-Wl,-syslibroot,/tmp/MacOSX10.11.sdk' \
     '-Wl,-dead_strip' \
--- libs/cross-compile-macos-on-linux-desktop-libs.sh
+++ libs/cross-compile-macos-on-linux-desktop-libs.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 
-# Install clang, a port of cctools, and the macOS SDK into /tmp.  This
+# Install clang, a port of cctools, and the macOS SDK into /tmp. This
 # is all cribbed from mozilla-central; start at
 # https://searchfox.org/mozilla-central/rev/39cb1e96cf97713c444c5a0404d4f84627aee85d/build/macosx/cross-mozconfig.common.
 
 set -euvx
 
+MANIFEST="${PWD}/macos-cc-tools.manifest"
+
 pushd /tmp
 
-curl --location --retry 10 --retry-delay 10 -o cross-clang.manifest https://hg.mozilla.org/mozilla-central/raw-file/588208caeaf863f2207792eeb1bd97e6c8fceed4/browser/config/tooltool-manifests/macosx64/cross-clang.manifest
-
-tooltool.py --manifest=cross-clang.manifest --url=http://taskcluster/tooltool.mozilla-releng.net/ fetch
-
-# curl --location --retry 10 --retry-delay 10 -o cctools.tar.xz https://index.taskcluster.net/v1/task/gecko.cache.level-3.toolchains.v2.linux64-cctools-port.latest/artifacts/public/build/cctools.tar.xz
-curl --location --retry 10 --retry-delay 10 -o cctools.tar.xz https://queue.taskcluster.net/v1/task/T-2QILzUSN-fEkRUH9bYvg/artifacts/public%2Fbuild%2Fcctools.tar.xz
-tar xf cctools.tar.xz
-
-# curl --location --retry 10 --retry-delay 10 -o clang.tar.xz https://index.taskcluster.net/v1/task/gecko.cache.level-3.toolchains.v2.linux64-clang-7.latest/artifacts/public/build/clang.tar.xz
-curl --location --retry 10 --retry-delay 10 -o clang.tar.xz https://queue.taskcluster.net/v1/task/a2942WbJRgObZFIDwRc_OQ/artifacts/public%2Fbuild%2Fclang.tar.xz
-tar xf clang.tar.xz
+tooltool.py \
+  --url=http://taskcluster/tooltool.mozilla-releng.net/ \
+  --manifest="${MANIFEST}" \
+  fetch
 
 popd
