a38e1734f314d95e5b04257ac296f665f90a52b4
Mitchell Hentges <mhentges@mozilla.com>
2019-10-08 16:30:47 -0700

    Runs module build on release

--- taskcluster/app_services_taskgraph/transforms/module_build.py
+++ taskcluster/app_services_taskgraph/transforms/module_build.py
@@ -11,11 +11,25 @@ transforms = TransformSequence()
 
 
 @transforms.add
+def release_upload_symbols(config, tasks):
+    for task in tasks:
+        if config.params["tasks_for"] != "github-release":
+            yield task
+            return
+
+        task["worker"]["script"] += "./automation/upload_android_symbols.sh {}".format(task["attributes"]["buildconfig"]["path"])
+        task["scopes"].append("secrets:get:project/application-services/symbols-token")
+        yield task
+
+
+@transforms.add
 def build_task(config, tasks):
     for task in tasks:
-        module_name = task["attributes"]["buildconfig"]["name"]
+        module_info = task["attributes"]["buildconfig"]
+        name = module_info["name"]
+        version = module_info["version"]
 
-        script = task["worker"].pop("script").format(module_name=module_name)
+        script = task["worker"].pop("script").format(module_name=name)
         bash_command = [
             "/bin/bash",
             "--login",
@@ -24,7 +38,19 @@ def build_task(config, tasks):
         ]
 
         task["run"]["command"] = bash_command
-        task["description"] = task["description"].format(module_name=module_name)
+        task["description"] = task["description"].format(module_name=name)
+        task["worker"]["artifacts"] = artifacts = []
+
+        for publication in module_info["publications"]:
+            primary_extensions = (".pom", ".aar", "-sources.jar") if publication["type"] == "aar" else (".pom", ".jar")
+            extensions = [package_ext + digest_ext for package_ext in primary_extensions for digest_ext in ("", ".sha1", ".md5")]
+            for extension in extensions:
+                artifact_filename = "{}-{}{}".format(name, version, extension)
+                artifacts.append({
+                    "name": "public/build/{}".format(artifact_filename),
+                    "path": "/builds/worker/checkouts/src/build/maven/org/mozilla/appservices/{}/{}/{}".format(name, version, artifact_filename),
+                    "type": "file",
+                })
+
 
         yield task
-
