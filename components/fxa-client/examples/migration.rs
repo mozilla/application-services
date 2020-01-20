5702fd5153b5f24fba9fa03b21d834f2977285a3
Vlad Filippov <vlad.filippov@gmail.com>
2020-01-20 11:02:22 -0500

    Add offline use case for 'migrateFromSessionToken'
    Fixes #2396

--- components/fxa-client/examples/migration.rs
+++ components/fxa-client/examples/migration.rs
@@ -1,11 +1,10 @@
 use cli_support::prompt::prompt_string;
 use fxa_client::FirefoxAccount;
+use std::{thread, time};
 
 static CLIENT_ID: &str = "3c49430b43dfba77";
-//static CONTENT_SERVER: &str = "https://latest.dev.lcip.org";
-static CONTENT_SERVER: &str = "http://127.0.0.1:3030";
-//static REDIRECT_URI: &str = "https://latest.dev.lcip.org/oauth/success/3c49430b43dfba77";
-static REDIRECT_URI: &str = "http://127.0.0.1:3030/oauth/success/3c49430b43dfba77";
+static CONTENT_SERVER: &str = "https://accounts.firefox.com";
+static REDIRECT_URI: &str = "https://accounts.firefox.com/oauth/success/3c49430b43dfba77";
 
 fn main() {
     let mut fxa = FirefoxAccount::new(CONTENT_SERVER, CLIENT_ID, REDIRECT_URI);
@@ -15,8 +14,22 @@ fn main() {
     let k_sync: String = prompt_string("k_sync").unwrap();
     println!("Enter kXCS (hex-string):");
     let k_xcs: String = prompt_string("k_xcs").unwrap();
-    fxa.migrate_from_session_token(&session_token, &k_sync, &k_xcs, true)
-        .unwrap();
-    println!("WOW! You've been migrated.");
+    let migration_result =
+        match fxa.migrate_from_session_token(&session_token, &k_sync, &k_xcs, true) {
+            Ok(migration_result) => migration_result,
+            Err(err) => {
+                println!("Error: {}", err);
+                // example for offline behaviour
+                loop {
+                    thread::sleep(time::Duration::from_millis(5000));
+                    let retry = fxa.try_migration();
+                    match retry {
+                        Ok(result) => break result,
+                        Err(_) => println!("Retrying... Are you connected to the internet?"),
+                    }
+                }
+            }
+        };
+    println!("WOW! You've been migrated in {:?}.", migration_result);
     println!("JSON: {}", fxa.to_json().unwrap());
 }
