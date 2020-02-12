5cf1f900d0b847527ba4025a9391015ae3981578
Edouard Oger <eoger@fastmail.com>
2020-02-12 11:36:24 -0500

    Add FxAccountManager.getSessionToken

--- components/fxa-client/ios/FxAClient/FxAccountOAuth.swift
+++ components/fxa-client/ios/FxAClient/FxAccountOAuth.swift
@@ -9,6 +9,8 @@ public enum OAuthScope {
     public static let profile: String = "profile"
     // Necessary to obtain sync keys.
     public static let oldSync: String = "https://identity.mozilla.com/apps/oldsync"
+    // Necessary to obtain a sessionToken, which gives full access to the account.
+    public static let session: String = "https://identity.mozilla.com/tokens/session"
 }
 
 public struct ScopedKey {
