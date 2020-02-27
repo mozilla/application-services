a80b88a580437770f758e4df1ef295a4f9a6b703
PepeRulo <joseraul.pastrana@gmail.com>
2020-02-26 22:00:29 -0600

    rolling back our erroneous modifications

--- components/fxa-client/src/oauth.rs
+++ components/fxa-client/src/oauth.rs
@@ -79,6 +79,14 @@ impl FirefoxAccount {
         Ok(token_info)
     }
 
+    /// Retrieve the current session token from state
+    pub fn get_session_token(&self) -> Result<String> {
+        match self.state.session_token {
+            Some(ref session_token) => Ok(session_token.to_string()),
+            None => Err(ErrorKind::NoSessionToken.into()),
+        }
+    }
+
     /// Check whether user is authorized using our refresh token.
     pub fn check_authorization_status(&self) -> Result<IntrospectInfo> {
         let resp = match self.state.refresh_token {
@@ -161,7 +169,7 @@ impl FirefoxAccount {
         state: &str,
         access_type: &str,
     ) -> Result<String> {
-        let session_token = self.state.session_token?;
+        let session_token = self.get_session_token()?;
         let resp = self.client.authorization_code_using_session_token(
             &self.state.config,
             &client_id,
