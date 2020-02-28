a388da0bfb79c36c49c7154d382130fa7d2742c1
Edouard Oger <eoger@fastmail.com>
2020-02-28 11:27:15 -0500

    Add --no-archive to build-carthage.sh
    XCode doesn't use the zip file, so we should be able to skip building it.

--- build-carthage.sh
+++ build-carthage.sh
@@ -4,10 +4,12 @@ set -eu
 
 CONFIGURATION="Release"
 FRAMEWORK_NAME="MozillaAppServices.framework.zip"
+ARCHIVE=true
 
 while [[ "$#" -gt 0 ]]; do case $1 in
   --configuration) CONFIGURATION="$2"; shift;shift;;
   --out) FRAMEWORK_NAME="$2"; shift;shift;;
+  --no-archive) ARCHIVE=false; shift;;
   *) echo "Unknown parameter: $1"; exit 1;
 esac; done
 
@@ -18,13 +20,16 @@ set -vx
 rustup update stable
 
 carthage bootstrap --platform iOS --cache-builds
-## When https://github.com/Carthage/Carthage/issues/2623 is fixed,
-## carthage build --archive should work to produce a zip
 
 set -o pipefail && \
 carthage build --no-skip-current --platform iOS --verbose --configuration "${CONFIGURATION}" --cache-builds | \
 tee raw_xcodebuild.log | \
 xcpretty
 
-# Exclude SwiftProtobuf.
-zip -r "${FRAMEWORK_NAME}" Carthage/Build/iOS megazords/ios/DEPENDENCIES.md -x '*SwiftProtobuf.framework*/*'
+if [ "$ARCHIVE" = true ]; then
+    ## When https://github.com/Carthage/Carthage/issues/2623 is fixed,
+    ## carthage build --archive should work to produce a zip
+
+    # Exclude SwiftProtobuf.
+    zip -r "${FRAMEWORK_NAME}" Carthage/Build/iOS megazords/ios/DEPENDENCIES.md -x '*SwiftProtobuf.framework*/*'
+fi
