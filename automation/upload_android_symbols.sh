71c736b465f48eb998079528b8fbca034d57c98f
Mihai Tabara <mtabara@mozilla.com>
2020-03-16 21:06:18 +0000

    Bug 1563169 - migrate PRs to use taskgraph instead
    Amendments to decisionlib
    
    Add TC toolchain scripts
    
    Add Docker image
    
    Add toolchain kind
    
    Add base tasks
    
    Add base toolchain transform
    
    Add single_dep transforms
    
    Add taskgraph loader and target_tasks
    
    Add rest of transforms
    
    Pip install per user
    
    Taskgraph-ify PR within .taskcluster.yml
    
    Fix trustDomain
    
    Revert pull-requests to public until support is added for privileged
    
    Add missing workerTypes module
    
    Add missing __init__ to load AS taskgraph transforms
    
    Switch to taskgraph-try for toolchain-script support
    
    Fix taskgraph trust-domain too
    
    Switch the sccache off
    
    Unpin rust version to stable
    
    Remove useless TODO
    
    Add comment to deal with sccache later
    
    Fix shellcheck linting
    
    Dummy commit to test TC CI graph
    
    Update taskgraph to tip
    
    Update to try-taskgraph first to ensure things work
    
    Remove head_tag as its defined already in taskgraph
    
    Use deepcopy
    
    Remove upload-symbols per release
    
    Move common to job-defaults
    
    Remove all references to head_tag
    
    Remove single_dep in favor of multi_dep
    
    Test some taskgraph changes before final review
    
    Use taskgraph revision
    
    Use fixed taskgraph revision

--- automation/upload_android_symbols.sh
+++ automation/upload_android_symbols.sh
@@ -48,5 +48,5 @@ for i in "${!TARGET_ARCHS[@]}"; do
 done
 
 # 2. Upload them.
-pip3 install -r automation/symbols-generation/requirements.txt
+pip3 install --user -r automation/symbols-generation/requirements.txt
 python3 automation/symbols-generation/upload_symbols.py "${OUTPUT_FOLDER}"
---
+++ taskcluster/app_services_taskgraph/target_tasks.py
@@ -0,0 +1,34 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from taskgraph.target_tasks import _target_task, filter_for_tasks_for
+
+
+@_target_task('pr-skip')
+def target_tasks_pr_skip(full_task_graph, parameters, graph_config):
+    return []
+
+
+@_target_task('pr-full')
+def target_tasks_default(full_task_graph, parameters, graph_config):
+    """Target the tasks which have indicated they should be run on this project
+    via the `run_on_projects` attributes."""
+    def filter(task):
+        return filter_for_tasks_for(task, parameters) \
+            and task.attributes.get("run-on-pr-type", "all") in ("full-ci", "all")
+
+    return [l for l, task in full_task_graph.tasks.iteritems() if filter(task)]
+
+
+@_target_task('pr-normal')
+def target_tasks_default(full_task_graph, parameters, graph_config):
+    """Target the tasks which have indicated they should be run on this project
+    via the `run_on_projects` attributes."""
+    def filter(task):
+        return filter_for_tasks_for(task, parameters) \
+                and task.attributes.get("run-on-pr-type", "all") in ("normal-ci", "all")
+
+    return [l for l, task in full_task_graph.tasks.iteritems() if filter(task)]
