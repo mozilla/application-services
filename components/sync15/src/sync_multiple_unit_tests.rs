4e1b5dea685cad2819a88ca719daa97c266a0881
PepeRulo <joseraul.pastrana@gmail.com>
2020-04-05 17:26:08 -0500

    began setting up unit tests for sync_multiple.rs

---
+++ components/sync15/src/sync_multiple_unit_tests.rs
@@ -0,0 +1,23 @@
+use crate::sync_multiple::{MemoryCachedState};
+use crate::client::{Sync15StorageClientInit};
+use crate::key_bundle::KeyBundle;
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_test() {
+        let mut mem_cached_state = MemoryCachedState{
+
+        }
+    }
+
+}
+
+// Dummy; empty.
+pub struct MockStore {
+}
+impl Store for MockStore {
+}
