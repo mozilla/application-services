04db95c3a5e4d9bbd0b94364618fb4c003b71ef2
Mihai Tabara <mtabara@mozilla.com>
2020-05-14 09:40:48 +0100

    Bug 1637695 - temporarily add old index to allow local buildinf of appsv

--- taskcluster/app_services_taskgraph/transforms/toolchain.py
+++ taskcluster/app_services_taskgraph/transforms/toolchain.py
@@ -4,10 +4,26 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
+import subprocess
+
 from taskgraph.transforms.base import TransformSequence
 from taskgraph.util.schema import resolve_keyed_by
 
 transforms = TransformSequence()
+
+# TODO: Bug 1637695 to be removed once we retire these old indexes
+TOOLCHAIN_OLD_INDEX = {
+    'android': 'index.project.application-services.application-services.build.libs.android.{sha}',
+    'desktop-linux': 'index.project.application-services.application-services.build.libs.desktop.linux.{sha}',
+    'desktop-macos': 'index.project.application-services.application-services.build.libs.desktop.macos.{sha}',
+    'desktop-win32-x86-64': 'index.project.application-services.application-services.build.libs.desktop.win32-x86-64.{sha}',
+}
+
+
+def git_sha_for_directory(directory):
+    output = subprocess.check_output(["git", "rev-parse", "HEAD:{}".format(directory)])
+    sha = output.decode("utf8").strip()
+    return sha
 
 
 @transforms.add
@@ -16,4 +32,9 @@ def resolve_keys(config, tasks):
         resolve_keyed_by(task, "routes", item_name=task["name"], **{
             "tasks-for": config.params["tasks_for"]
         })
+        # TODO: Bug 1637695 - temp solution to unblock local building of
+        # appplication-services. Once we switch to new indexes, we should clean this up
+        sha = git_sha_for_directory("libs")
+        routes = task['routes']
+        routes.append(TOOLCHAIN_OLD_INDEX[task['name']].format(sha=sha))
         yield task
