446f919f7f2d34300c566957abcd1ad66b068052
Mihai Tabara <mtabara@mozilla.com>
2020-05-27 15:30:45 +0100

    Bug 1563169 - automation improvements prerequisite for Github releases (#3113)

--- taskcluster/app_services_taskgraph/transforms/module_build.py
+++ taskcluster/app_services_taskgraph/transforms/module_build.py
@@ -6,6 +6,9 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from taskgraph.transforms.base import TransformSequence
 
+from ..build_config import get_version, get_extensions
+
+
 transforms = TransformSequence()
 
 
@@ -14,17 +17,17 @@ def build_task(config, tasks):
     for task in tasks:
         module_info = task["attributes"]["buildconfig"]
         name = module_info["name"]
-        version = module_info["version"]
+        version = get_version()
 
-        task["worker"]["script"] = task["worker"]["script"].format(module_name=name)
+        for i,item in enumerate(task["run"]["gradlew"]):
+            task["run"]["gradlew"][i] = task["run"]["gradlew"][i].format(module_name=name)
         task["description"] = task["description"].format(module_name=name)
         task["worker"]["artifacts"] = artifacts = []
 
-        for publication in module_info["publications"]:
-            primary_extensions = (".pom", ".aar", "-sources.jar") if publication["type"] == "aar" else (".pom", ".jar")
-            extensions = [package_ext + digest_ext for package_ext in primary_extensions for digest_ext in ("", ".sha1", ".md5")]
+        all_extensions = get_extensions(name)
+        for publication_name, extensions in all_extensions.items():
             for extension in extensions:
-                artifact_filename = "{}-{}{}".format(publication["name"], version, extension)
+                artifact_filename = "{}-{}{}".format(publication_name, version, extension)
                 artifacts.append({
                     "name": "public/build/{}".format(artifact_filename),
                     "path": "/builds/worker/checkouts/src/build/maven/org/mozilla/appservices/{}/{}/{}".format(name, version, artifact_filename),
