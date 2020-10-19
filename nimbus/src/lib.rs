2a780774599c6a12cf1d6b442b0b1a80febb01f7
Edouard Oger <eoger@fastmail.com>
2020-10-19 10:04:40 -0400

    Avoid I/O in constructor

--- nimbus/src/lib.rs
+++ nimbus/src/lib.rs
@@ -14,16 +14,17 @@ mod sampling;
 #[cfg(debug_assertions)]
 pub use evaluator::evaluate_enrollment;
 
-use ::uuid::Uuid;
 pub use config::RemoteSettingsConfig;
 use enrollment::{
     get_enrollments, opt_in_with_branch, opt_out, reset_enrollment, update_enrollments,
 };
 use http_client::{Client, SettingsClient};
 pub use matcher::AppContext;
+use once_cell::sync::OnceCell;
 use persistence::{Database, StoreId};
 use serde_derive::*;
-use std::path::Path;
+use std::path::PathBuf;
+use uuid::Uuid;
 
 const DEFAULT_TOTAL_BUCKETS: u32 = 10000;
 const DB_KEY_NIMBUS_ID: &str = "nimbus-id";
@@ -35,12 +36,14 @@ pub struct NimbusClient {
     http_client: Client,
     available_randomization_units: AvailableRandomizationUnits,
     app_context: AppContext,
-    db: Database,
-    nimbus_id: Uuid,
+    db: OnceCell<Database>,
+    db_path: PathBuf,
 }
 
 impl NimbusClient {
-    pub fn new<P: AsRef<Path>>(
+    // This constructor *must* not do any kind of I/O since it might be called on the main
+    // thread in the gecko Javascript stack, hence the use of OnceCell for the db.
+    pub fn new<P: Into<PathBuf>>(
         collection_name: String,
         app_context: AppContext,
         db_path: P,
@@ -48,14 +51,12 @@ impl NimbusClient {
         available_randomization_units: AvailableRandomizationUnits,
     ) -> Result<Self> {
         let http_client = Client::new(&collection_name, config)?;
-        let db = Database::new(db_path)?;
-        let nimbus_id = Self::get_or_create_nimbus_id(&db)?;
         Ok(Self {
             http_client,
             available_randomization_units,
             app_context,
-            db,
-            nimbus_id,
+            db_path: db_path.into(),
+            db: OnceCell::default(),
         })
     }
 
@@ -65,7 +66,7 @@ impl NimbusClient {
     // But for now, if we have no experiments we assume we have never
     // successfully hit our server, so should do that now.
     fn maybe_initial_experiment_fetch(&self) -> Result<()> {
-        if !self.db.has_any(StoreId::Experiments)? {
+        if !self.db()?.has_any(StoreId::Experiments)? {
             log::info!("No experiments in our database - fetching them");
             self.update_experiments()?;
         }
@@ -82,27 +83,27 @@ impl NimbusClient {
 
     pub fn get_active_experiments(&self) -> Result<Vec<EnrolledExperiment>> {
         self.maybe_initial_experiment_fetch()?;
-        get_enrollments(&self.db)
+        get_enrollments(self.db()?)
     }
 
     pub fn get_all_experiments(&self) -> Result<Vec<Experiment>> {
         self.maybe_initial_experiment_fetch()?;
-        self.db.collect_all(StoreId::Experiments)
+        self.db()?.collect_all(StoreId::Experiments)
     }
 
     pub fn opt_in_with_branch(&self, experiment_slug: String, branch: String) -> Result<()> {
-        opt_in_with_branch(&self.db, &experiment_slug, &branch)
+        opt_in_with_branch(self.db()?, &experiment_slug, &branch)
     }
 
     pub fn opt_out(&self, experiment_slug: String) -> Result<()> {
-        opt_out(&self.db, &experiment_slug)
+        opt_out(self.db()?, &experiment_slug)
     }
 
     pub fn reset_enrollment(&self, experiment_slug: String) -> Result<()> {
         reset_enrollment(
-            &self.db,
+            self.db()?,
             &experiment_slug,
-            &self.nimbus_id,
+            &self.nimbus_id()?,
             &self.available_randomization_units,
             &self.app_context,
         )
@@ -115,34 +116,35 @@ impl NimbusClient {
         let experiments = self.http_client.get_experiments()?;
         // XXX - we need transaction support but it's not clear how to expose
         // that support.
-        self.db.clear(StoreId::Experiments)?;
+        self.db()?.clear(StoreId::Experiments)?;
         for experiment in experiments {
             log::debug!("found experiment {}", experiment.slug);
-            self.db
+            self.db()?
                 .put(StoreId::Experiments, &experiment.slug, &experiment)?;
         }
         // Now update all enrollments based on the new set.
         update_enrollments(
-            &self.db,
-            &self.nimbus_id,
+            self.db()?,
+            &self.nimbus_id()?,
             &self.available_randomization_units,
             &self.app_context,
         )
     }
 
-    pub fn nimbus_id(&self) -> Uuid {
-        self.nimbus_id
-    }
-
-    fn get_or_create_nimbus_id(db: &Database) -> Result<Uuid> {
-        Ok(match db.get(StoreId::Meta, DB_KEY_NIMBUS_ID)? {
+    pub fn nimbus_id(&self) -> Result<Uuid> {
+        Ok(match self.db()?.get(StoreId::Meta, DB_KEY_NIMBUS_ID)? {
             Some(nimbus_id) => nimbus_id,
             None => {
                 let nimbus_id = Uuid::new_v4();
-                db.put(StoreId::Meta, DB_KEY_NIMBUS_ID, &nimbus_id)?;
+                self.db()?
+                    .put(StoreId::Meta, DB_KEY_NIMBUS_ID, &nimbus_id)?;
                 nimbus_id
             }
         })
+    }
+
+    fn db(&self) -> Result<&Database> {
+        self.db.get_or_try_init(|| Database::new(&self.db_path))
     }
 }
 
@@ -171,7 +173,7 @@ pub struct Experiment {
     pub proposed_duration: Option<u32>,
     pub proposed_enrollment: u32,
     pub reference_branch: Option<String>,
-    // N.B. eecords in RemoteSettings will have `id` and `filter_expression` fields,
+    // N.B. records in RemoteSettings will have `id` and `filter_expression` fields,
     // but we ignore them because they're for internal use by RemoteSettings.
 }
 
