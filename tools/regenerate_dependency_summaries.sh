006c1d659daad12641f7a4294d3be524ef666739
Ryan Kelly <ryan@rfk.id.au>
2020-11-07 12:30:53 +1100

    Remove the "lockbox" custom megazord.
    The lockwise apps are on a very old version of appservices and I don't believe
    we have a plan for updating them anytime soon. Continuing to build the lockbox
    megazord does not seem like a good use of CI or local dev resources.
    
    We understand how to bring this back if we need it, but also, if we do need
    it again in future it's probably a good opportunity to revisit how we build
    custom megazords in the first place.

--- tools/regenerate_dependency_summaries.sh
+++ tools/regenerate_dependency_summaries.sh
@@ -8,6 +8,3 @@ python3 ./tools/dependency_summary.py --all-ios-targets --package megazord_ios >
 
 python3 ./tools/dependency_summary.py --all-android-targets --package megazord > megazords/full/DEPENDENCIES.md
 python3 ./tools/dependency_summary.py --all-android-targets --package megazord --format pom > megazords/full/android/dependency-licenses.xml
-
-python3 ./tools/dependency_summary.py --all-android-targets --package lockbox > megazords/lockbox/DEPENDENCIES.md
-python3 ./tools/dependency_summary.py --all-android-targets --package lockbox --format pom > megazords/lockbox/android/dependency-licenses.xml
