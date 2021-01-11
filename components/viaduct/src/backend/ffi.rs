880221f62474b78d0a5de507866860ac7a603dfd
Ryan Kelly <ryan@rfk.id.au>
2021-01-11 11:37:04 +1100

    Fix Rust Beta warning about deprecated `AtomicBool` method.
    When compiling on Rust Beta, we're generating a warning about:
    
      warning: use of deprecated associated function
      `std::sync::atomic::AtomicBool::compare_and_swap`:
      Use `compare_exchange` or `compare_exchange_weak` instead
    
    This commit does as advised in the hope of eliminating the warning.

--- components/viaduct/src/backend/ffi.rs
+++ components/viaduct/src/backend/ffi.rs
@@ -154,15 +154,17 @@ mod callback_holder {
     /// Set the function pointer to the FetchCallback. Returns false if we did nothing because the callback had already been initialized
     pub(super) fn set_callback(h: FetchCallback) -> bool {
         let as_usize = h as usize;
-        let old_ptr = CALLBACK_PTR.compare_and_swap(0, as_usize, Ordering::SeqCst);
-        if old_ptr != 0 {
-            // This is an internal bug, the other side of the FFI should ensure
-            // it sets this only once. Note that this is actually going to be
-            // before logging is initialized in practice, so there's not a lot
-            // we can actually do here.
-            log::error!("Bug: Initialized CALLBACK_PTR multiple times");
+        match CALLBACK_PTR.compare_exchange(0, as_usize, Ordering::SeqCst, Ordering::SeqCst) {
+            Ok(_) => true,
+            Err(_) => {
+                // This is an internal bug, the other side of the FFI should ensure
+                // it sets this only once. Note that this is actually going to be
+                // before logging is initialized in practice, so there's not a lot
+                // we can actually do here.
+                log::error!("Bug: Initialized CALLBACK_PTR multiple times");
+                false
+            }
         }
-        old_ptr == 0
     }
 }
 
