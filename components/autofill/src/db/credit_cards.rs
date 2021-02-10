bc70bafa116953cbb4da596e68d950888f36737f
lougeniaC64 <lougeniaC64@users.noreply.github.com>
2021-02-09 19:50:50 -0500

    Implemented core autofill sync15 logic (#3845)

--- components/autofill/src/db/credit_cards.rs
+++ components/autofill/src/db/credit_cards.rs
@@ -3,11 +3,18 @@
 * file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-use crate::db::models::credit_card::{InternalCreditCard, UpdatableCreditCardFields};
-use crate::db::schema::{CREDIT_CARD_COMMON_COLS, CREDIT_CARD_COMMON_VALS};
+use crate::db::{
+    models::credit_card::{InternalCreditCard, UpdatableCreditCardFields},
+    schema::{CREDIT_CARD_COMMON_COLS, CREDIT_CARD_COMMON_VALS},
+    store::{delete_meta, put_meta},
+};
 use crate::error::*;
+use crate::sync::credit_card::engine::{
+    COLLECTION_SYNCID_META_KEY, GLOBAL_SYNCID_META_KEY, LAST_SYNC_META_KEY,
+};
 
-use rusqlite::{Connection, NO_PARAMS};
+use rusqlite::{Connection, Transaction, NO_PARAMS};
+use sync15::EngineSyncAssociation;
 use sync_guid::Guid;
 use types::Timestamp;
 
@@ -163,10 +170,163 @@ pub fn touch(conn: &Connection, guid: &Guid) -> Result<()> {
     Ok(())
 }
 
+pub fn reset_in_tx(tx: &Transaction<'_>, assoc: &EngineSyncAssociation) -> Result<()> {
+    // Remove all synced credit cards and pending tombstones, and mark all
+    // local credit cards as new.
+    tx.execute_batch(
+        "DELETE FROM credit_cards_mirror;
+
+        DELETE FROM credit_cards_tombstones;
+
+        UPDATE credit_cards_data
+        SET sync_change_counter = 1",
+    )?;
+
+    // Reset the last sync time, so that the next sync fetches fresh records
+    // from the server.
+    put_meta(tx, LAST_SYNC_META_KEY, &0)?;
+
+    // Clear the sync ID if we're signing out, or set it to whatever the
+    // server gave us if we're signing in.
+    match assoc {
+        EngineSyncAssociation::Disconnected => {
+            delete_meta(tx, GLOBAL_SYNCID_META_KEY)?;
+            delete_meta(tx, COLLECTION_SYNCID_META_KEY)?;
+        }
+        EngineSyncAssociation::Connected(ids) => {
+            put_meta(tx, GLOBAL_SYNCID_META_KEY, &ids.global)?;
+            put_meta(tx, COLLECTION_SYNCID_META_KEY, &ids.coll)?;
+        }
+    }
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::db::test::new_mem_db;
+    use crate::db::{store::get_meta, test::new_mem_db};
+    use sql_support::ConnExt;
+    use sync15::CollSyncIds;
+
+    fn get_all(
+        conn: &Connection,
+        table_name: String,
+    ) -> rusqlite::Result<Vec<String>, rusqlite::Error> {
+        let mut stmt = conn.prepare(&format!(
+            "SELECT guid FROM {table_name}",
+            table_name = table_name
+        ))?;
+        let rows = stmt.query_map(NO_PARAMS, |row| row.get(0))?;
+
+        let mut guids = Vec::new();
+        for guid_result in rows {
+            guids.push(guid_result?);
+        }
+
+        Ok(guids)
+    }
+
+    fn clear_tables(conn: &Connection) -> rusqlite::Result<(), rusqlite::Error> {
+        conn.execute_all(&[
+            "DELETE FROM credit_cards_data;",
+            "DELETE FROM credit_cards_mirror;",
+            "DELETE FROM credit_cards_tombstones;",
+            "DELETE FROM moz_meta;",
+        ])
+    }
+
+    fn insert_tombstone_record(
+        conn: &Connection,
+        guid: String,
+    ) -> rusqlite::Result<usize, rusqlite::Error> {
+        conn.execute_named(
+            "INSERT OR IGNORE INTO credit_cards_tombstones (
+                guid,
+                time_deleted
+            ) VALUES (
+                :guid,
+                :time_deleted
+            )",
+            rusqlite::named_params! {
+                ":guid": guid,
+                ":time_deleted": Timestamp::now(),
+            },
+        )
+    }
+
+    fn insert_mirror_record(
+        conn: &Connection,
+        credit_card: &InternalCreditCard,
+    ) -> rusqlite::Result<usize, rusqlite::Error> {
+        conn.execute_named(
+            "INSERT OR IGNORE INTO credit_cards_mirror (
+                guid,
+                cc_name,
+                cc_number,
+                cc_exp_month,
+                cc_exp_year,
+                cc_type,
+                time_created,
+                time_last_used,
+                time_last_modified,
+                times_used
+            ) VALUES (
+                :guid,
+                :cc_name,
+                :cc_number,
+                :cc_exp_month,
+                :cc_exp_year,
+                :cc_type,
+                :time_created,
+                :time_last_used,
+                :time_last_modified,
+                :times_used
+            )",
+            rusqlite::named_params! {
+                ":guid": credit_card.guid,
+                ":cc_name": credit_card.cc_name,
+                ":cc_number": credit_card.cc_number,
+                ":cc_exp_month": credit_card.cc_exp_month,
+                ":cc_exp_year": credit_card.cc_exp_year,
+                ":cc_type": credit_card.cc_type,
+                ":time_created": credit_card.time_created,
+                ":time_last_used": credit_card.time_last_used,
+                ":time_last_modified": credit_card.time_last_modified,
+                ":times_used": credit_card.times_used,
+            },
+        )
+    }
+
+    fn insert_record(
+        conn: &Connection,
+        credit_card: &InternalCreditCard,
+    ) -> rusqlite::Result<usize, rusqlite::Error> {
+        conn.execute_named(
+            &format!(
+                "INSERT OR IGNORE INTO credit_cards_data (
+                    {common_cols}
+                ) VALUES (
+                    {common_vals}
+                )",
+                common_cols = CREDIT_CARD_COMMON_COLS,
+                common_vals = CREDIT_CARD_COMMON_VALS,
+            ),
+            rusqlite::named_params! {
+                ":guid": credit_card.guid,
+                ":cc_name": credit_card.cc_name,
+                ":cc_number": credit_card.cc_number,
+                ":cc_exp_month": credit_card.cc_exp_month,
+                ":cc_exp_year": credit_card.cc_exp_year,
+                ":cc_type": credit_card.cc_type,
+                ":time_created": credit_card.time_created,
+                ":time_last_used": credit_card.time_last_used,
+                ":time_last_modified": credit_card.time_last_modified,
+                ":times_used": credit_card.times_used,
+                ":sync_change_counter": credit_card.sync_change_counter,
+            },
+        )
+    }
 
     #[test]
     fn test_credit_card_create_and_read() -> Result<()> {
@@ -347,43 +507,7 @@ mod tests {
         )?;
 
         // create a mirror record to check that a tombstone record is created upon deletion
-        db.execute_named(
-            "INSERT OR IGNORE INTO credit_cards_mirror (
-                guid,
-                cc_name,
-                cc_number,
-                cc_exp_month,
-                cc_exp_year,
-                cc_type,
-                time_created,
-                time_last_used,
-                time_last_modified,
-                times_used
-            ) VALUES (
-                :guid,
-                :cc_name,
-                :cc_number,
-                :cc_exp_month,
-                :cc_exp_year,
-                :cc_type,
-                :time_created,
-                :time_last_used,
-                :time_last_modified,
-                :times_used
-            )",
-            rusqlite::named_params! {
-                ":guid": saved_credit_card2.guid,
-                ":cc_name": saved_credit_card2.cc_name,
-                ":cc_number": saved_credit_card2.cc_number,
-                ":cc_exp_month": saved_credit_card2.cc_exp_month,
-                ":cc_exp_year": saved_credit_card2.cc_exp_year,
-                ":cc_type": saved_credit_card2.cc_type,
-                ":time_created": saved_credit_card2.time_created,
-                ":time_last_used": saved_credit_card2.time_last_used,
-                ":time_last_modified": saved_credit_card2.time_last_modified,
-                ":times_used": saved_credit_card2.times_used,
-            },
-        )?;
+        insert_mirror_record(&db, &saved_credit_card2)?;
 
         let delete_result2 = delete_credit_card(&db, &saved_credit_card2.guid);
         assert!(delete_result2.is_ok());
@@ -414,25 +538,12 @@ mod tests {
     }
 
     #[test]
-    fn test_credit_card_trigger_on_create() {
+    fn test_credit_card_trigger_on_create() -> Result<()> {
         let db = new_mem_db();
         let guid = Guid::random();
 
         // create a tombstone record
-        let tombstone_result = db.execute_named(
-            "INSERT OR IGNORE INTO credit_cards_tombstones (
-                guid,
-                time_deleted
-            ) VALUES (
-                :guid,
-                :time_deleted
-            )",
-            rusqlite::named_params! {
-                ":guid": guid.as_str(),
-                ":time_deleted": Timestamp::now(),
-            },
-        );
-        assert!(tombstone_result.is_ok());
+        insert_tombstone_record(&db, guid.to_string())?;
 
         // create a new credit card with the tombstone's guid
         let credit_card = InternalCreditCard {
@@ -446,30 +557,7 @@ mod tests {
             ..Default::default()
         };
 
-        let add_credit_card_result = db.execute_named(
-            &format!(
-                "INSERT OR IGNORE INTO credit_cards_data (
-                    {common_cols}
-                ) VALUES (
-                    {common_vals}
-                )",
-                common_cols = CREDIT_CARD_COMMON_COLS,
-                common_vals = CREDIT_CARD_COMMON_VALS,
-            ),
-            rusqlite::named_params! {
-                ":guid": credit_card.guid,
-                ":cc_name": credit_card.cc_name,
-                ":cc_number": credit_card.cc_number,
-                ":cc_exp_month": credit_card.cc_exp_month,
-                ":cc_exp_year": credit_card.cc_exp_year,
-                ":cc_type": credit_card.cc_type,
-                ":time_created": credit_card.time_created,
-                ":time_last_used": credit_card.time_last_used,
-                ":time_last_modified": credit_card.time_last_modified,
-                ":times_used": credit_card.times_used,
-                ":sync_change_counter": credit_card.sync_change_counter,
-            },
-        );
+        let add_credit_card_result = insert_record(&db, &credit_card);
         assert!(add_credit_card_result.is_err());
 
         let expected_error_message = "guid exists in `credit_cards_tombstones`";
@@ -477,10 +565,12 @@ mod tests {
             expected_error_message,
             add_credit_card_result.unwrap_err().to_string()
         );
+
+        Ok(())
     }
 
     #[test]
-    fn test_credit_card_trigger_on_delete() {
+    fn test_credit_card_trigger_on_delete() -> Result<()> {
         let db = new_mem_db();
         let guid = Guid::random();
 
@@ -494,54 +584,18 @@ mod tests {
             cc_type: "visa".to_string(),
             ..Default::default()
         };
-
-        let add_credit_card_result = db.execute_named(
-            &format!(
-                "INSERT OR IGNORE INTO credit_cards_data (
-                    {common_cols}
-                ) VALUES (
-                    {common_vals}
-                )",
-                common_cols = CREDIT_CARD_COMMON_COLS,
-                common_vals = CREDIT_CARD_COMMON_VALS,
-            ),
-            rusqlite::named_params! {
-                ":guid": credit_card.guid,
-                ":cc_name": credit_card.cc_name,
-                ":cc_number": credit_card.cc_number,
-                ":cc_exp_month": credit_card.cc_exp_month,
-                ":cc_exp_year": credit_card.cc_exp_year,
-                ":cc_type": credit_card.cc_type,
-                ":time_created": credit_card.time_created,
-                ":time_last_used": credit_card.time_last_used,
-                ":time_last_modified": credit_card.time_last_modified,
-                ":times_used": credit_card.times_used,
-                ":sync_change_counter": credit_card.sync_change_counter,
-            },
-        );
-        assert!(add_credit_card_result.is_ok());
+        insert_record(&db, &credit_card)?;
 
         // create a tombstone record with the same guid
-        let tombstone_result = db.execute_named(
-            "INSERT OR IGNORE INTO credit_cards_tombstones (
-                guid,
-                time_deleted
-            ) VALUES (
-                :guid,
-                :time_deleted
-            )",
-            rusqlite::named_params! {
-                ":guid": credit_card.guid,
-                ":time_deleted": Timestamp::now(),
-            },
-        );
-        assert!(tombstone_result.is_err());
+        let tombstone_result = insert_tombstone_record(&db, credit_card.guid.to_string());
 
         let expected_error_message = "guid exists in `credit_cards_data`";
         assert_eq!(
             expected_error_message,
             tombstone_result.unwrap_err().to_string()
         );
+
+        Ok(())
     }
 
     #[test]
@@ -567,6 +621,107 @@ mod tests {
 
         assert_eq!(touched_credit_card.sync_change_counter, 2);
         assert_eq!(touched_credit_card.times_used, 1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_credit_card_sync_reset() -> Result<()> {
+        let mut db = new_mem_db();
+        let tx = &db.transaction()?;
+
+        // create a record
+        let credit_card = InternalCreditCard {
+            guid: Guid::random(),
+            cc_name: "jane doe".to_string(),
+            cc_number: "2222333344445555".to_string(),
+            cc_exp_month: 3,
+            cc_exp_year: 2022,
+            cc_type: "visa".to_string(),
+
+            ..InternalCreditCard::default()
+        };
+        insert_record(&tx, &credit_card)?;
+
+        // create a mirror record
+        let mirror_record = InternalCreditCard {
+            guid: Guid::random(),
+            cc_name: "jane doe".to_string(),
+            cc_number: "2222333344445555".to_string(),
+            cc_exp_month: 3,
+            cc_exp_year: 2022,
+            cc_type: "visa".to_string(),
+
+            ..InternalCreditCard::default()
+        };
+        insert_mirror_record(&tx, &mirror_record)?;
+
+        // create a tombstone record
+        let tombstone_guid = Guid::random();
+        insert_tombstone_record(&tx, tombstone_guid.to_string())?;
+
+        // create sync metadata
+        let global_guid = Guid::new("AAAA");
+        let coll_guid = Guid::new("AAAA");
+        let ids = CollSyncIds {
+            global: global_guid.clone(),
+            coll: coll_guid.clone(),
+        };
+        put_meta(&tx, GLOBAL_SYNCID_META_KEY, &ids.global)?;
+        put_meta(&tx, COLLECTION_SYNCID_META_KEY, &ids.coll)?;
+
+        // call reset for sign out
+        reset_in_tx(&tx, &EngineSyncAssociation::Disconnected)?;
+
+        // check that sync change counter has been reset
+        let reset_record_exists: bool = tx.query_row(
+            "SELECT EXISTS (
+                SELECT 1
+                FROM credit_cards_data
+                WHERE sync_change_counter = 1
+            )",
+            NO_PARAMS,
+            |row| row.get(0),
+        )?;
+        assert!(reset_record_exists);
+
+        // check that the mirror and tombstone tables have no records
+        assert!(get_all(&tx, "credit_cards_mirror".to_string())?.is_empty());
+        assert!(get_all(&tx, "credit_cards_tombstones".to_string())?.is_empty());
+
+        // check that the last sync time was reset to 0
+        let expected_sync_time = 0;
+        assert_eq!(
+            get_meta::<i64>(&tx, LAST_SYNC_META_KEY)?.unwrap_or(1),
+            expected_sync_time
+        );
+
+        // check that the meta records were deleted
+        assert!(get_meta::<String>(&tx, GLOBAL_SYNCID_META_KEY)?.is_none());
+        assert!(get_meta::<String>(&tx, COLLECTION_SYNCID_META_KEY)?.is_none());
+
+        clear_tables(&tx)?;
+
+        // re-populating the tables
+        insert_record(&tx, &credit_card)?;
+        insert_mirror_record(&tx, &mirror_record)?;
+        insert_tombstone_record(&tx, tombstone_guid.to_string())?;
+
+        // call reset for sign in
+        reset_in_tx(&tx, &EngineSyncAssociation::Connected(ids))?;
+
+        // check that the meta records were set
+        let retrieved_global_sync_id = get_meta::<String>(&tx, GLOBAL_SYNCID_META_KEY)?;
+        assert_eq!(
+            retrieved_global_sync_id.unwrap_or_default(),
+            global_guid.to_string()
+        );
+
+        let retrieved_coll_sync_id = get_meta::<String>(&tx, COLLECTION_SYNCID_META_KEY)?;
+        assert_eq!(
+            retrieved_coll_sync_id.unwrap_or_default(),
+            coll_guid.to_string()
+        );
 
         Ok(())
     }
