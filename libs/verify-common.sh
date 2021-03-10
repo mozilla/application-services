23874c50322599ed08ec91dc8b2985de7650a6ea
Ryan Kelly <ryan@rfk.id.au>
2021-03-10 16:30:46 +1100

    Remove an orphaned comment about installing uniffi-bindgen.

--- libs/verify-common.sh
+++ libs/verify-common.sh
@@ -7,8 +7,6 @@ if ! [[ -x "$(command -v rustc)" ]]; then
   exit 1
 fi
 
-echo "Installing uniffi_bindgen if missing; it's necessary for binding generation during the builds."
-
 # Print the rustc version (we don't update it because our CI and official
 # builds we will often be pinned on an ealier rust version, but should still
 # work OK with later ones.)
