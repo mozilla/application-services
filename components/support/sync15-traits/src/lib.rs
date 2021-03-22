1587211b71caf58118e705c6899b3a36ce12262c
Mark Hammond <mhammond@skippinet.com.au>
2021-03-22 14:44:00 +1100

    Improve sync15-traits and BridgedEngine docs

--- components/support/sync15-traits/src/lib.rs
+++ components/support/sync15-traits/src/lib.rs
@@ -2,6 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+//! # sync15-traits
+//!
+//! The sync15-traits support component is a home for the core types and traits
+//! used by Sync. It's extracted into its own crate because these types are used
+//! in so many different contexts, from the [Sync Manager](../sync_manager/index.html)
+//! to desktop Firefox.
+//!
+//! Because this is a "support" component, there aren't any instructions for how
+//! to use this component, but you will find documentation for most of the types
+//! and traits implemented here.
+
 #![warn(rust_2018_idioms)]
 pub mod bridged_engine;
 mod changeset;
