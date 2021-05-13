f6dfc96e1173663eb77e7ae26193b6e82abf5d8d
Dan Mosedale <dmose2@gmail.com>
2021-05-12 17:14:03 -0700

    Add remaining 2 test cases

--- components/nimbus/src/persistence.rs
+++ components/nimbus/src/persistence.rs
@@ -824,6 +824,94 @@ mod tests {
                 "id":"valid-feature-experiment", // change when cloning
                 "last_modified":1_602_197_324_372i64
             }),
+            json!({
+                "schemaVersion": "1.0.0",
+                "slug": "branch-feature-feature-id-empty", // change when cloning
+                "endDate": null,
+                "featureIds": [""], // change when cloning
+                "branches":[
+                    {
+                        "slug": "control",
+                        "ratio": 1,
+                        "feature": {
+                            "featureId": "", // change when cloning
+                            "enabled": false
+                        }
+                    },
+                    {
+                        "slug": "treatment",
+                        "ratio":1,
+                        "feature": {
+                            "featureId": "", // change when cloning
+                            "enabled": true
+                        }
+                    }
+                ],
+                "channel": "nightly",
+                "probeSets":[],
+                "startDate":null,
+                "appName": "fenix",
+                "appId": "org.mozilla.fenix",
+                "bucketConfig":{
+                    // Setup to enroll everyone by default.
+                    "count":10_000,
+                    "start":0,
+                    "total":10_000,
+                    "namespace":"branch-feature-feature-id-empty", // change when cloning
+                    "randomizationUnit":"nimbus_id"
+                },
+                "userFacingName":"Diagnostic test experiment",
+                "referenceBranch":"control",
+                "isEnrollmentPaused":false,
+                "proposedEnrollment":7,
+                "userFacingDescription":"This is a test experiment for diagnostic purposes.",
+                "id":"branch-feature-feature-id-empty", // change when cloning
+                "last_modified":1_602_197_324_372i64
+            }),
+            json!({
+                "schemaVersion": "1.0.0",
+                "slug": "branch-feature-value-missing", // change when cloning // XXX verify that we really to discard these and that there aren't any live experiments with have this problem.  If so, clean up all remaining experiments in this list
+                "endDate": null,
+                "featureIds": ["ggg"], // change when cloning
+                "branches":[
+                    {
+                        "slug": "control",
+                        "ratio": 1,
+                        "feature": {
+                            "featureId": "ggg", // change when cloning
+                            "enabled": false
+                        }
+                    },
+                    {
+                        "slug": "treatment",
+                        "ratio":1,
+                        "feature": {
+                            "featureId": "ggg", // change when cloning
+                            "enabled": true
+                        }
+                    }
+                ],
+                "channel": "nightly",
+                "probeSets":[],
+                "startDate":null,
+                "appName": "fenix",
+                "appId": "org.mozilla.fenix",
+                "bucketConfig":{
+                    // Setup to enroll everyone by default.
+                    "count":10_000,
+                    "start":0,
+                    "total":10_000,
+                    "namespace":"branch_feature_value_missing", // change when cloning
+                    "randomizationUnit":"nimbus_id"
+                },
+                "userFacingName":"Diagnostic test experiment",
+                "referenceBranch":"control",
+                "isEnrollmentPaused":false,
+                "proposedEnrollment":7,
+                "userFacingDescription":"This is a test experiment for diagnostic purposes.",
+                "id":"branch_feature_value_missing", // change when cloning
+                "last_modified":1_602_197_324_372i64
+            }),
         ]
     }
 
@@ -859,7 +947,7 @@ mod tests {
 
         // write a bunch of invalid experiments
         let invalid_feature_experiments = &get_invalid_feature_experiments();
-        assert_eq!(6, invalid_feature_experiments.len());
+        assert_eq!(8, invalid_feature_experiments.len());
 
         for experiment in invalid_feature_experiments {
             log::debug!("experiment = {:?}", experiment);
