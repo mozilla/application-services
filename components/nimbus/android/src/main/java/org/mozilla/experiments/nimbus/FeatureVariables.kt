746625b63dabdffaf7a4df3b67ff79ade68fffda
James Hugman <james@hugman.tv>
2021-05-17 19:00:31 +0100

    Add drawable and translatable strings to android

--- components/nimbus/android/src/main/java/org/mozilla/experiments/nimbus/FeatureVariables.kt
+++ components/nimbus/android/src/main/java/org/mozilla/experiments/nimbus/FeatureVariables.kt
@@ -5,34 +5,109 @@
 package org.mozilla.experiments.nimbus
 
 import android.content.Context
-import android.graphics.Color
+import android.graphics.drawable.Drawable
 import org.json.JSONObject
 
 /**
- * It provides the type-coercion tooling and resource look-up to be immediately useful to feature
- * developers.
+ * `Variables` provides a type safe key-value style interface to configure application features
  *
  * The feature developer requests a typed value with a specific `key`. If the key is present, and
  * the value is of the correct type, then it is returned. If neither of these are true, then `null`
  * is returned.
  *
+ * Supported types:
+ *
+ * Basic:
+ *
+ *  - `String`
+ *  - `Int`
+ *  - `Boolean`
+ *
+ * Resource types. These use `getString` to look up an experiment value, then the app's `Context` to
+ * find a resource.
+ *
+ *  - `StringResource`
+ *  - `DrawableResource`
+ *
  * ```
- * val config = nimbus.getFeatureVariables("submitButton")
+ * val config = nimbus.getVariables("submitButton")
  *
  * submitButton.text = config.getText("submitButton.text") ?: R.string.submit_button_label
  * submitButton.color = config.getColor("submitButton.color") ?: R.color.button_default
  *
  * ```
  *
- * This may become the basis of a generated-from-manifest solution.
+ * Each of the keys needed by feature should be documented in the app's experiment manifest, which
+ * will provide enough information the Experimenter to design an experiment.
  */
 interface Variables {
-    fun getString(key: String): String?
-    fun getInt(key: String): Int?
-    fun getBool(key: String): Boolean?
+    /**
+     * Finds a string typed value for this key. If none exists, `null` is returned.
+     *
+     * N.B. the `key` and type `String` should be listed in the experiment manifest.
+     */
+    fun getString(key: String): String? = null
+
+    /**
+     * Finds a integer typed value for this key. If none exists, `null` is returned.
+     *
+     * N.B. the `key` and type `Int` should be listed in the experiment manifest.
+     */
+    fun getInt(key: String): Int? = null
+
+    /**
+     * Finds a boolean typed value for this key. If none exists, `null` is returned.
+     *
+     * N.B. the `key` and type `String` should be listed in the experiment manifest.
+     */
+    fun getBool(key: String): Boolean? = null
+
+    /**
+     * Uses `getString(key: String)` to find the name of a drawable resource. If no value for `key`
+     * exists, or no resource named with that value exists, then `null` is returned.
+     *
+     * N.B. the `key` and type `Image` should be listed in the experiment manifest. The
+     * names of the drawable resources should also be listed.
+     */
+    fun getDrawableResource(key: String): Int? = null
+
+    /**
+     * Uses `getString(key: String)` to find the name of a string resource. If no value for `key`
+     * exists, or no resource named with that value exists, then `null` is returned.
+     *
+     * N.B. the `key` and type `LocalizedString` should be listed in the experiment manifest. The
+     * names of the string resources should also be listed.
+     */
+    fun getStringResource(key: String): Int? = null
+
+    /**
+     * Uses `getString(key: String)` to find the name of a string resource. If a value exists, and
+     * a string resource exists with that name, then returns the string from the resource. If no
+     * such resource exists, then return the string value as the text.
+     *
+     * This is a shorthand for:
+     *
+     * ```
+     * val text: String? = nimbus.getString(key)?.let(context::getString) ?: nimbus.getString(key)
+     * ```
+     *
+     * For strings, this is almost always the right choice.
+     *
+     * N.B. the `key` and type `LocalizedString` should be listed in the experiment manifest. The
+     * names of the string resources should also be listed.
+     */
+    fun getText(key: String): String? = null
+
+    /**
+     * Convenience method for `getDrawableResource(key)?.let { context.getDrawable(it) }`.
+     *
+     * N.B. the `key` and type `Image` should be listed in the experiment manifest. The
+     * names of the drawable resources should also be listed.
+     */
+    fun getDrawable(key: String): Drawable? = null
 
     // Get a child configuration object.
-    fun getVariables(key: String): Variables?
+    fun getVariables(key: String): Variables? = null
     // This may be important when transforming in to a code generated object.
     fun <T> getVariables(key: String, transform: (Variables) -> T) = getVariables(key)?.let(transform)
 }
@@ -41,20 +116,18 @@ interface VariablesWithContext : Variables {
     val context: Context
     // Lower level accessors that can come across the FFI.
     // Platform specific types, deserialized from the lower level types.
-    fun getColor(key: String) = getString(key)?.let(this::asColor)
-    fun getTextResource(key: String, context: Context = this.context): Int? = getString(key)?.let {
-        context.getResource(it, "string")
-    }
-    fun getText(key: String, context: Context = this.context) = getString(key)?.let(this::asText)
-    fun getDrawableResource(key: String, context: Context = this.context) = getString(key)?.let(this::asDrawableResource)
+    override fun getDrawableResource(key: String) = getString(key)?.let(this::asDrawableResource)
+    override fun getStringResource(key: String): Int? = getString(key)?.let(this::asStringResource)
+    override fun getText(key: String) = getString(key)?.let(this::asText)
+    override fun getDrawable(key: String) = getDrawableResource(key)?.let(this::asDrawable)
 
     // These `as*` methods become useful when transforming values found in JSON to actual values
     // the app will use. They're broken out here so they can be re-used by codegen generating
     // defaults from manifest information.
-    fun asColor(string: String) = Color.parseColor(string)
     fun asText(res: Int) = context.getString(res)
-    fun asText(string: String) = context.getResource(string, "string")?.let(this::asText)
-        ?: string
+    fun asDrawable(res: Int) = context.getDrawable(res)
+    fun asText(string: String) = asStringResource(string)?.let(this::asText) ?: string
+    fun asStringResource(string: String) = context.getResource(string, "string")
     fun asDrawableResource(string: String) = context.getResource(string, "drawable")
 }
 
@@ -104,12 +177,4 @@ class NullVariables : Variables {
     companion object {
         val instance = NullVariables()
     }
-
-    override fun getString(key: String): String? = null
-
-    override fun getInt(key: String): Int? = null
-
-    override fun getBool(key: String): Boolean? = null
-
-    override fun getVariables(key: String): Variables? = null
 }
