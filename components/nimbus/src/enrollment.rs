a9c5c2cad7644033462b772b68c9f94c427f83e4
Dan Mosedale <dmose2@gmail.com>
2021-06-07 14:43:37 -0700

    Now really get it passing

--- components/nimbus/src/enrollment.rs
+++ components/nimbus/src/enrollment.rs
@@ -714,13 +714,24 @@ impl<'a> EnrollmentsEvolver<'a> {
                     }
                 )
             {
-                let next_enrollment = self.evolve_enrollment(
+                log::debug!("about to call evolve_enrollment (2nd call site)");
+
+                let next_enrollment = match self.evolve_enrollment(
                     is_user_participating,
                     prev_experiments.get(slug).copied(),
                     Some(next_experiment),
                     prev_enrollment,
                     &mut enrollment_events,
-                )?;
+                ) {
+                    Ok(enrollment) => enrollment,
+                    Err(e) => {
+                        // XXX test that any associated enrollments,
+                        // prev_experiments?, next_experiments also dropped
+                        // XXX YYY finish this logging
+                        log::error!("evolve_enrollment(\n\tprev_exp: {:?}\n\t, next_exp: {:?}, \n\tprev_enrollment: {:?})\n\t returned an error: {}; dropping this record", prev_experiments.get(slug).copied(), Some(next_experiment), prev_enrollment, e);
+                        None
+                    }
+                };
 
                 if let Some(enrollment) = next_enrollment {
                     // We get the FeatureConfig out of the enrollment.
