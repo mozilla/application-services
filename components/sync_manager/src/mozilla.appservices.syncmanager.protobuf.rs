1264d50223be99bde844ea09434a6379109bda45
Tarik Eshaq <teshaq@mozilla.com>
2021-07-09 12:21:57 -0700

    Updates cargo dependencies and bumps prost to 0.8

--- components/sync_manager/src/mozilla.appservices.syncmanager.protobuf.rs
+++ components/sync_manager/src/mozilla.appservices.syncmanager.protobuf.rs
@@ -1,33 +1,33 @@
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct SyncParams {
     #[prost(string, repeated, tag="1")]
-    pub engines_to_sync: ::std::vec::Vec<std::string::String>,
+    pub engines_to_sync: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
     #[prost(bool, required, tag="2")]
     pub sync_all_engines: bool,
     #[prost(enumeration="SyncReason", required, tag="3")]
     pub reason: i32,
     #[prost(map="string, bool", tag="4")]
-    pub engines_to_change_state: ::std::collections::HashMap<std::string::String, bool>,
+    pub engines_to_change_state: ::std::collections::HashMap<::prost::alloc::string::String, bool>,
     #[prost(string, optional, tag="5")]
-    pub persisted_state: ::std::option::Option<std::string::String>,
+    pub persisted_state: ::core::option::Option<::prost::alloc::string::String>,
     /// These conceptually are a nested type, but exposing them as such would add
     /// needless complexity to the FFI.
     #[prost(string, required, tag="6")]
-    pub acct_key_id: std::string::String,
+    pub acct_key_id: ::prost::alloc::string::String,
     #[prost(string, required, tag="7")]
-    pub acct_access_token: std::string::String,
+    pub acct_access_token: ::prost::alloc::string::String,
     #[prost(string, required, tag="8")]
-    pub acct_tokenserver_url: std::string::String,
+    pub acct_tokenserver_url: ::prost::alloc::string::String,
     #[prost(string, required, tag="9")]
-    pub acct_sync_key: std::string::String,
+    pub acct_sync_key: ::prost::alloc::string::String,
     #[prost(string, required, tag="10")]
-    pub fxa_device_id: std::string::String,
+    pub fxa_device_id: ::prost::alloc::string::String,
     #[prost(string, required, tag="11")]
-    pub device_name: std::string::String,
+    pub device_name: ::prost::alloc::string::String,
     #[prost(enumeration="DeviceType", required, tag="12")]
     pub device_type: i32,
     #[prost(map="string, string", tag="13")]
-    pub local_encryption_keys: ::std::collections::HashMap<std::string::String, std::string::String>,
+    pub local_encryption_keys: ::std::collections::HashMap<::prost::alloc::string::String, ::prost::alloc::string::String>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct SyncResult {
@@ -35,18 +35,18 @@ pub struct SyncResult {
     pub status: i32,
     /// empty string used for 'no error'
     #[prost(map="string, string", tag="2")]
-    pub results: ::std::collections::HashMap<std::string::String, std::string::String>,
+    pub results: ::std::collections::HashMap<::prost::alloc::string::String, ::prost::alloc::string::String>,
     #[prost(string, repeated, tag="3")]
-    pub declined: ::std::vec::Vec<std::string::String>,
+    pub declined: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
     /// false if we didn't manage to check declined.
     #[prost(bool, required, tag="4")]
     pub have_declined: bool,
     #[prost(int64, optional, tag="5")]
-    pub next_sync_allowed_at: ::std::option::Option<i64>,
+    pub next_sync_allowed_at: ::core::option::Option<i64>,
     #[prost(string, required, tag="6")]
-    pub persisted_state: std::string::String,
+    pub persisted_state: ::prost::alloc::string::String,
     #[prost(string, optional, tag="7")]
-    pub telemetry_json: ::std::option::Option<std::string::String>,
+    pub telemetry_json: ::core::option::Option<::prost::alloc::string::String>,
 }
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
--- components/tabs/src/mozilla.appservices.remotetabs.protobuf.rs
+++ components/tabs/src/mozilla.appservices.remotetabs.protobuf.rs
@@ -1,28 +1,28 @@
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ClientTabs {
     #[prost(string, required, tag="1")]
-    pub client_id: std::string::String,
+    pub client_id: ::prost::alloc::string::String,
     #[prost(message, repeated, tag="2")]
-    pub remote_tabs: ::std::vec::Vec<RemoteTab>,
+    pub remote_tabs: ::prost::alloc::vec::Vec<RemoteTab>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ClientsTabs {
     #[prost(message, repeated, tag="1")]
-    pub clients_tabs: ::std::vec::Vec<ClientTabs>,
+    pub clients_tabs: ::prost::alloc::vec::Vec<ClientTabs>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RemoteTab {
     #[prost(string, required, tag="1")]
-    pub title: std::string::String,
+    pub title: ::prost::alloc::string::String,
     #[prost(string, repeated, tag="2")]
-    pub url_history: ::std::vec::Vec<std::string::String>,
+    pub url_history: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
     #[prost(string, optional, tag="3")]
-    pub icon: ::std::option::Option<std::string::String>,
+    pub icon: ::core::option::Option<::prost::alloc::string::String>,
     #[prost(int64, required, tag="4")]
     pub last_used: i64,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RemoteTabs {
     #[prost(message, repeated, tag="1")]
-    pub remote_tabs: ::std::vec::Vec<RemoteTab>,
+    pub remote_tabs: ::prost::alloc::vec::Vec<RemoteTab>,
 }
