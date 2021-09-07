575d17ac4b8fa16a4a0471a2db27d588b940d6e6
Mark Hammond <mhammond@skippinet.com.au>
2021-09-07 11:04:58 +1000

    Explicitly link sqlsupport against NSS, allowing its tests to be enabled.

--- components/support/sql/src/open_database.rs
+++ components/support/sql/src/open_database.rs
@@ -196,225 +196,221 @@ pub mod test_utils {
     }
 }
 
-// Disabled because it breaks `cargo test` from the root director because of NSS linking issues.
-// It seems that any test code that calls one of the `rusqlite::Connection::open*` calls causes
-// this.
-//
-// #[cfg(test)]
-// mod test {
-//     use super::test_utils::MigratedDatabaseFile;
-//     use super::*;
-//     use std::cell::RefCell;
-//
-//     struct TestMigrationLogic {
-//         pub calls: RefCell<Vec<&'static str>>,
-//         pub buggy_v3_upgrade: bool,
-//     }
-//
-//     impl TestMigrationLogic {
-//         pub fn new() -> Self {
-//             Self {
-//                 calls: RefCell::new(Vec::new()),
-//                 buggy_v3_upgrade: false,
-//             }
-//         }
-//         pub fn new_with_buggy_logic() -> Self {
-//             Self {
-//                 calls: RefCell::new(Vec::new()),
-//                 buggy_v3_upgrade: true,
-//             }
-//         }
-//
-//         pub fn clear_calls(&self) {
-//             self.calls.borrow_mut().clear();
-//         }
-//
-//         pub fn push_call(&self, call: &'static str) {
-//             self.calls.borrow_mut().push(call);
-//         }
-//
-//         pub fn check_calls(&self, expected: Vec<&'static str>) {
-//             assert_eq!(*self.calls.borrow(), expected);
-//         }
-//     }
-//
-//     impl MigrationLogic for TestMigrationLogic {
-//         const NAME: &'static str = "test db";
-//         const END_VERSION: u32 = 4;
-//
-//         fn setup_pragmas(&self, conn: &Connection) -> Result<()> {
-//             conn.execute_batch("PRAGMA journal_mode = wal;")?;
-//             Ok(())
-//         }
-//
-//         fn prepare(&self, conn: &Connection) -> Result<()> {
-//             self.push_call("prep");
-//             conn.execute_batch(
-//                 "
-//                 CREATE TABLE prep_table(col);
-//                 INSERT INTO prep_table(col) VALUES ('correct-value');
-//                 ",
-//             )?;
-//             Ok(())
-//         }
-//
-//         fn init(&self, conn: &Connection) -> Result<()> {
-//             self.push_call("init");
-//             conn.execute_batch(
-//                 "
-//                 CREATE TABLE my_table(col);
-//                 ",
-//             )
-//             .map_err(|e| e.into())
-//         }
-//
-//         fn upgrade_from(&self, conn: &Connection, version: u32) -> Result<()> {
-//             match version {
-//                 2 => {
-//                     self.push_call("upgrade_from_v2");
-//                     conn.execute_batch(
-//                         "
-//                         ALTER TABLE my_old_table_name RENAME TO my_table;
-//                         ",
-//                     )?;
-//                     Ok(())
-//                 }
-//                 3 => {
-//                     self.push_call("upgrade_from_v3");
-//
-//                     if self.buggy_v3_upgrade {
-//                         conn.execute_batch("ILLEGAL_SQL_CODE")?;
-//                     }
-//
-//                     conn.execute_batch(
-//                         "
-//                         ALTER TABLE my_table RENAME COLUMN old_col to col;
-//                         ",
-//                     )?;
-//                     Ok(())
-//                 }
-//                 _ => {
-//                     panic!("Unexpected version: {}", version);
-//                 }
-//             }
-//         }
-//
-//         fn finish(&self, conn: &Connection) -> Result<()> {
-//             self.push_call("finish");
-//             conn.execute_batch(
-//                 "
-//                 INSERT INTO my_table(col) SELECT col FROM prep_table;
-//                 DROP TABLE prep_table;
-//                 ",
-//             )?;
-//             Ok(())
-//         }
-//     }
-//
-//     // Initialize the database to v2 to test upgrading from there
-//     static INIT_V2: &str = "
-//         CREATE TABLE my_old_table_name(old_col);
-//         PRAGMA user_version=2;
-//     ";
-//
-//     fn check_final_data(conn: &Connection) {
-//         let value: String = conn
-//             .query_row("SELECT col FROM my_table", NO_PARAMS, |r| r.get(0))
-//             .unwrap();
-//         assert_eq!(value, "correct-value");
-//         assert_eq!(get_schema_version(&conn).unwrap(), 4);
-//     }
-//
-//     #[test]
-//     fn test_init() {
-//         let migration_logic = TestMigrationLogic::new();
-//         let conn = open_memory_database(&migration_logic).unwrap();
-//         check_final_data(&conn);
-//         migration_logic.check_calls(vec!["prep", "init", "finish"]);
-//     }
-//
-//     #[test]
-//     fn test_upgrades() {
-//         let db_file = MigratedDatabaseFile::new(TestMigrationLogic::new(), INIT_V2);
-//         let conn = open_database(db_file.path.clone(), &db_file.migration_logic).unwrap();
-//         check_final_data(&conn);
-//         db_file.migration_logic.check_calls(vec![
-//             "prep",
-//             "upgrade_from_v2",
-//             "upgrade_from_v3",
-//             "finish",
-//         ]);
-//     }
-//
-//     #[test]
-//     fn test_open_current_version() {
-//         let db_file = MigratedDatabaseFile::new(TestMigrationLogic::new(), INIT_V2);
-//         db_file.upgrade_to(4);
-//         db_file.migration_logic.clear_calls();
-//         let conn = open_database(db_file.path.clone(), &db_file.migration_logic).unwrap();
-//         check_final_data(&conn);
-//         db_file.migration_logic.check_calls(vec!["prep", "finish"]);
-//     }
-//
-//     #[test]
-//     fn test_pragmas() {
-//         let db_file = MigratedDatabaseFile::new(TestMigrationLogic::new(), INIT_V2);
-//         let conn = open_database(db_file.path.clone(), &db_file.migration_logic).unwrap();
-//         assert_eq!(
-//             conn.query_one::<String>("PRAGMA journal_mode").unwrap(),
-//             "wal"
-//         );
-//     }
-//
-//     #[test]
-//     fn test_migration_error() {
-//         let db_file =
-//             MigratedDatabaseFile::new(TestMigrationLogic::new_with_buggy_logic(), INIT_V2);
-//         db_file
-//             .open()
-//             .execute(
-//                 "INSERT INTO my_old_table_name(old_col) VALUES ('I should not be deleted')",
-//                 NO_PARAMS,
-//             )
-//             .unwrap();
-//
-//         open_database(db_file.path.clone(), &db_file.migration_logic).unwrap_err();
-//         // Even though the upgrades failed, the data should still be there.  The changes that
-//         // upgrade_to_v3 made should have been rolled back.
-//         assert_eq!(
-//             db_file
-//                 .open()
-//                 .query_one::<i32>("SELECT COUNT(*) FROM my_old_table_name")
-//                 .unwrap(),
-//             1
-//         );
-//     }
-//
-//     #[test]
-//     fn test_version_too_new() {
-//         let db_file = MigratedDatabaseFile::new(TestMigrationLogic::new(), INIT_V2);
-//         set_schema_version(&db_file.open(), 5).unwrap();
-//
-//         db_file
-//             .open()
-//             .execute(
-//                 "INSERT INTO my_old_table_name(old_col) VALUES ('I should not be deleted')",
-//                 NO_PARAMS,
-//             )
-//             .unwrap();
-//
-//         assert!(matches!(
-//             open_database(db_file.path.clone(), &db_file.migration_logic,),
-//             Err(Error::IncompatibleVersion(5))
-//         ));
-//         // Make sure that even when DeleteAndRecreate is specified, we don't delete the database
-//         // file when the schema is newer
-//         assert_eq!(
-//             db_file
-//                 .open()
-//                 .query_one::<i32>("SELECT COUNT(*) FROM my_old_table_name")
-//                 .unwrap(),
-//             1
-//         );
-//     }
-// }
+#[cfg(test)]
+mod test {
+    use super::test_utils::MigratedDatabaseFile;
+    use super::*;
+    use std::cell::RefCell;
+
+    struct TestMigrationLogic {
+        pub calls: RefCell<Vec<&'static str>>,
+        pub buggy_v3_upgrade: bool,
+    }
+
+    impl TestMigrationLogic {
+        pub fn new() -> Self {
+            Self {
+                calls: RefCell::new(Vec::new()),
+                buggy_v3_upgrade: false,
+            }
+        }
+        pub fn new_with_buggy_logic() -> Self {
+            Self {
+                calls: RefCell::new(Vec::new()),
+                buggy_v3_upgrade: true,
+            }
+        }
+
+        pub fn clear_calls(&self) {
+            self.calls.borrow_mut().clear();
+        }
+
+        pub fn push_call(&self, call: &'static str) {
+            self.calls.borrow_mut().push(call);
+        }
+
+        pub fn check_calls(&self, expected: Vec<&'static str>) {
+            assert_eq!(*self.calls.borrow(), expected);
+        }
+    }
+
+    impl MigrationLogic for TestMigrationLogic {
+        const NAME: &'static str = "test db";
+        const END_VERSION: u32 = 4;
+
+        fn setup_pragmas(&self, conn: &Connection) -> Result<()> {
+            conn.execute_batch("PRAGMA journal_mode = wal;")?;
+            Ok(())
+        }
+
+        fn prepare(&self, conn: &Connection) -> Result<()> {
+            self.push_call("prep");
+            conn.execute_batch(
+                "
+                CREATE TABLE prep_table(col);
+                INSERT INTO prep_table(col) VALUES ('correct-value');
+                ",
+            )?;
+            Ok(())
+        }
+
+        fn init(&self, conn: &Connection) -> Result<()> {
+            self.push_call("init");
+            conn.execute_batch(
+                "
+                CREATE TABLE my_table(col);
+                ",
+            )
+            .map_err(|e| e.into())
+        }
+
+        fn upgrade_from(&self, conn: &Connection, version: u32) -> Result<()> {
+            match version {
+                2 => {
+                    self.push_call("upgrade_from_v2");
+                    conn.execute_batch(
+                        "
+                        ALTER TABLE my_old_table_name RENAME TO my_table;
+                        ",
+                    )?;
+                    Ok(())
+                }
+                3 => {
+                    self.push_call("upgrade_from_v3");
+
+                    if self.buggy_v3_upgrade {
+                        conn.execute_batch("ILLEGAL_SQL_CODE")?;
+                    }
+
+                    conn.execute_batch(
+                        "
+                        ALTER TABLE my_table RENAME COLUMN old_col to col;
+                        ",
+                    )?;
+                    Ok(())
+                }
+                _ => {
+                    panic!("Unexpected version: {}", version);
+                }
+            }
+        }
+
+        fn finish(&self, conn: &Connection) -> Result<()> {
+            self.push_call("finish");
+            conn.execute_batch(
+                "
+                INSERT INTO my_table(col) SELECT col FROM prep_table;
+                DROP TABLE prep_table;
+                ",
+            )?;
+            Ok(())
+        }
+    }
+
+    // Initialize the database to v2 to test upgrading from there
+    static INIT_V2: &str = "
+        CREATE TABLE my_old_table_name(old_col);
+        PRAGMA user_version=2;
+    ";
+
+    fn check_final_data(conn: &Connection) {
+        let value: String = conn
+            .query_row("SELECT col FROM my_table", NO_PARAMS, |r| r.get(0))
+            .unwrap();
+        assert_eq!(value, "correct-value");
+        assert_eq!(get_schema_version(&conn).unwrap(), 4);
+    }
+
+    #[test]
+    fn test_init() {
+        let migration_logic = TestMigrationLogic::new();
+        let conn = open_memory_database(&migration_logic).unwrap();
+        check_final_data(&conn);
+        migration_logic.check_calls(vec!["prep", "init", "finish"]);
+    }
+
+    #[test]
+    fn test_upgrades() {
+        let db_file = MigratedDatabaseFile::new(TestMigrationLogic::new(), INIT_V2);
+        let conn = open_database(db_file.path.clone(), &db_file.migration_logic).unwrap();
+        check_final_data(&conn);
+        db_file.migration_logic.check_calls(vec![
+            "prep",
+            "upgrade_from_v2",
+            "upgrade_from_v3",
+            "finish",
+        ]);
+    }
+
+    #[test]
+    fn test_open_current_version() {
+        let db_file = MigratedDatabaseFile::new(TestMigrationLogic::new(), INIT_V2);
+        db_file.upgrade_to(4);
+        db_file.migration_logic.clear_calls();
+        let conn = open_database(db_file.path.clone(), &db_file.migration_logic).unwrap();
+        check_final_data(&conn);
+        db_file.migration_logic.check_calls(vec!["prep", "finish"]);
+    }
+
+    #[test]
+    fn test_pragmas() {
+        let db_file = MigratedDatabaseFile::new(TestMigrationLogic::new(), INIT_V2);
+        let conn = open_database(db_file.path.clone(), &db_file.migration_logic).unwrap();
+        assert_eq!(
+            conn.query_one::<String>("PRAGMA journal_mode").unwrap(),
+            "wal"
+        );
+    }
+
+    #[test]
+    fn test_migration_error() {
+        let db_file =
+            MigratedDatabaseFile::new(TestMigrationLogic::new_with_buggy_logic(), INIT_V2);
+        db_file
+            .open()
+            .execute(
+                "INSERT INTO my_old_table_name(old_col) VALUES ('I should not be deleted')",
+                NO_PARAMS,
+            )
+            .unwrap();
+
+        open_database(db_file.path.clone(), &db_file.migration_logic).unwrap_err();
+        // Even though the upgrades failed, the data should still be there.  The changes that
+        // upgrade_to_v3 made should have been rolled back.
+        assert_eq!(
+            db_file
+                .open()
+                .query_one::<i32>("SELECT COUNT(*) FROM my_old_table_name")
+                .unwrap(),
+            1
+        );
+    }
+
+    #[test]
+    fn test_version_too_new() {
+        let db_file = MigratedDatabaseFile::new(TestMigrationLogic::new(), INIT_V2);
+        set_schema_version(&db_file.open(), 5).unwrap();
+
+        db_file
+            .open()
+            .execute(
+                "INSERT INTO my_old_table_name(old_col) VALUES ('I should not be deleted')",
+                NO_PARAMS,
+            )
+            .unwrap();
+
+        assert!(matches!(
+            open_database(db_file.path.clone(), &db_file.migration_logic,),
+            Err(Error::IncompatibleVersion(5))
+        ));
+        // Make sure that even when DeleteAndRecreate is specified, we don't delete the database
+        // file when the schema is newer
+        assert_eq!(
+            db_file
+                .open()
+                .query_one::<i32>("SELECT COUNT(*) FROM my_old_table_name")
+                .unwrap(),
+            1
+        );
+    }
+}
