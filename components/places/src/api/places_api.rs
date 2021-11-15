344412bd0e2d6b6db58b8cb9815526b4f1d55427
Ben Dean-Kawamura <bdk@mozilla.com>
2021-11-15 16:55:50 -0500

    Adding some comments about the state of sync interruption

--- components/places/src/api/places_api.rs
+++ components/places/src/api/places_api.rs
@@ -283,6 +283,8 @@ impl PlacesApi {
                     mem_cached_state,
                     client_init,
                     key_bundle,
+                    // The interrupt system is not currently working (#1684), but we need to pass
+                    // in something, so let's use the engine scope.
                     &engine.scope,
                     None,
                 )
@@ -305,6 +307,8 @@ impl PlacesApi {
                     mem_cached_state,
                     client_init,
                     key_bundle,
+                    // The interrupt system is not currently working (#1684), but we need to pass
+                    // in something, so let's use the engine scope.
                     &engine.scope,
                     None,
                 )
@@ -386,6 +390,8 @@ impl PlacesApi {
             &mut mem_cached_state,
             client_init,
             key_bundle,
+            // The interrupt system is not currently working (#1684), but we need to pass
+            // in something, so let's use the engine scope.
             &bm_engine.scope,
             None,
         );
