df930507f695c9c2dd8113aae75571444b2cbc5a
Tarik Eshaq <teshaq@mozilla.com>
2021-12-06 12:52:00 -0800

    Fixes swiftformat

--- components/fxa-client/ios/FxAClient/FxAccountManager.swift
+++ components/fxa-client/ios/FxAClient/FxAccountManager.swift
@@ -52,9 +52,7 @@ open class FxAccountManager {
         setupInternalListeners()
     }
 
-    private lazy var statePersistenceCallback: FxAStatePersistenceCallback = {
-        FxAStatePersistenceCallback(manager: self)
-    }()
+    private lazy var statePersistenceCallback: FxAStatePersistenceCallback = .init(manager: self)
 
     /// Starts the FxA account manager and advances the state machine.
     /// It is required to call this method before doing anything else with the manager.
