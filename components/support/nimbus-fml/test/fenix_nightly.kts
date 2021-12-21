a3e4de3f8f185fc70a7530718130627dbec301e8
Tarik Eshaq <teshaq@mozilla.com>
2021-12-21 14:44:21 -0700

    Adds nimbus fml default validation (#4744)
    * Adds default validation
    
    * Apply suggestions from code review
    
    Co-authored-by: Travis Long <tlong@mozilla.com>
    
    * Adds fenix and fenix nightly tests
    
    Co-authored-by: Travis Long <tlong@mozilla.com>

---
+++ components/support/nimbus-fml/test/fenix_nightly.kts
@@ -0,0 +1,50 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import org.mozilla.experiments.nimbus.MockNimbus
+
+// Test the default map with an enum to Boolean maping based
+// on the nighlty defaults
+val feature = MyNimbus.features.homescreen.value()
+assert(feature.sectionsEnabled[HomeScreenSection.TOP_SITES] == true)
+assert(feature.sectionsEnabled[HomeScreenSection.JUMP_BACK_IN] == true)
+assert(feature.sectionsEnabled[HomeScreenSection.RECENTLY_SAVED] == true)
+assert(feature.sectionsEnabled[HomeScreenSection.RECENT_EXPLORATIONS] == true)
+assert(feature.sectionsEnabled[HomeScreenSection.POCKET] == true)
+
+// Test whether we can selectively override the property based default.
+val api = MockNimbus("homescreen" to """{
+    "sections-enabled": {
+        "pocket": false
+    }
+}""", "nimbus-validation" to """{
+    "settings-title": "hello"
+}""", "search-term-groups" to """{
+    "enabled": true
+}""")
+MyNimbus.api = api
+val feature1 = MyNimbus.features.homescreen.value()
+assert(feature1.sectionsEnabled[HomeScreenSection.TOP_SITES] == true)
+assert(feature1.sectionsEnabled[HomeScreenSection.JUMP_BACK_IN] == true)
+assert(feature1.sectionsEnabled[HomeScreenSection.RECENTLY_SAVED] == true)
+assert(feature1.sectionsEnabled[HomeScreenSection.RECENT_EXPLORATIONS] == true)
+assert(feature1.sectionsEnabled[HomeScreenSection.POCKET] == false)
+
+// Record the exposure and test it.
+MyNimbus.features.homescreen.recordExposure()
+assert(api.isExposed("homescreen"))
+
+val validationFeature = MyNimbus.features.nimbusValidation.value()
+assert(validationFeature.settingsTitle == "hello")
+assert(validationFeature.settingsPunctuation == "")
+assert(validationFeature.settingsIcon == "mozac_ic_settings")
+// Record the exposure and test it.
+MyNimbus.features.nimbusValidation.recordExposure()
+assert(api.isExposed("nimbus-validation"))
+
+val searchTermGroupsFeature = MyNimbus.features.searchTermGroups.value()
+assert(searchTermGroupsFeature.enabled == true)
+
+MyNimbus.features.searchTermGroups.recordExposure()
+assert(api.isExposed("search-term-groups"))
