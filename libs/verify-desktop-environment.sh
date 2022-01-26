7e450299062b3725cde76b0696346187daa73685
Sammy Khamis <skhamis@mozilla.com>
2022-01-26 12:47:05 -1000

    updated verify desktop scripts to help people migrate

--- libs/verify-desktop-environment.sh
+++ libs/verify-desktop-environment.sh
@@ -6,6 +6,9 @@
 
 set -e
 
+yellow=`tput setaf 3`
+reset=`tput sgr0`
+
 if [[ ! -f "$(pwd)/libs/build-all.sh" ]]; then
   echo "ERROR: bootstrap-desktop.sh should be run from the root directory of the repo"
   exit 1
@@ -14,7 +17,11 @@ fi
 "$(pwd)/libs/verify-common.sh"
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
-  APPSERVICES_PLATFORM_DIR="$(pwd)/libs/desktop/darwin"
+  if [[ "$(uname -m)" == "arm64" ]]; then
+    APPSERVICES_PLATFORM_DIR="$(pwd)/libs/desktop/darwin-aarch64"
+  else
+    APPSERVICES_PLATFORM_DIR="$(pwd)/libs/desktop/darwin-x86-64"
+  fi
 else
   APPSERVICES_PLATFORM_DIR="$(pwd)/libs/desktop/linux-x86-64"
 fi
@@ -22,6 +29,20 @@ fi
 if [[ -z "${SQLCIPHER_LIB_DIR}" ]] || [[ -z "${SQLCIPHER_INCLUDE_DIR}" ]] || [[ -z "${NSS_DIR}" ]] || [[ -z "${NSS_STATIC}" ]]; then
   echo "Some necessary environment variables are not set."
   echo "Please export or add to your shell initialization file (.zshenv, .bashrc etc.) the following:"
+  echo ""
+  echo "export SQLCIPHER_LIB_DIR=${APPSERVICES_PLATFORM_DIR}/sqlcipher/lib"
+  echo "export SQLCIPHER_INCLUDE_DIR=${APPSERVICES_PLATFORM_DIR}/sqlcipher/include"
+  echo "export NSS_DIR=${APPSERVICES_PLATFORM_DIR}/nss"
+  echo "export NSS_STATIC=1"
+  exit 1
+fi
+
+# If users previously have built NSS their env vars will still be pointing to darwin
+# we need to tell them to update their env with the new arch-specific style
+if [[ "${NSS_DIR}" != *"desktop/darwin-"*  ]]; then
+  echo ""
+  echo "${yellow}Your environment variables are outdated! Please use the updated values below"
+  echo "Please export or add to your shell initialization file (.zshenv, .bashrc etc.) the following:${reset}"
   echo ""
   echo "export SQLCIPHER_LIB_DIR=${APPSERVICES_PLATFORM_DIR}/sqlcipher/lib"
   echo "export SQLCIPHER_INCLUDE_DIR=${APPSERVICES_PLATFORM_DIR}/sqlcipher/include"
