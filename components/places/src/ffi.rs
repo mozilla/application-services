8f1a8e61c0d8a812e5c8dab3bf7eeb2f0d783d63
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-01-27 01:25:34 -0300

    removed unused code from issue #4719

--- components/places/src/ffi.rs
+++ components/places/src/ffi.rs
@@ -383,25 +383,6 @@ impl PlacesConnection {
         })
     }
 
-    // XXX - We probably need to document/name this a little better as it's specifically for
-    // history and NOT bookmarks...
-    fn wipe_local_history(&self) -> Result<()> {
-        self.with_conn(history::wipe_local)
-    }
-
-    // Calls wipe_local_history but also updates the
-    // sync metadata to only sync after most recent visit to prevent
-    // further syncing of older data
-    fn delete_everything_history(&self) -> Result<()> {
-        self.with_conn(history::delete_everything)
-    }
-
-    // XXX - This just calls wipe_local under the hood...
-    // should probably have this go away?
-    fn prune_destructively(&self) -> Result<()> {
-        self.with_conn(history::prune_destructively)
-    }
-
     fn run_maintenance(&self) -> Result<()> {
         self.with_conn(storage::run_maintenance)
     }
