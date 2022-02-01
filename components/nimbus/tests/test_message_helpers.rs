5981b5a9d049d7fb23875440e83d53c000d1ed91
James Hugman <james@hugman.tv>
2022-02-01 19:19:06 +0000

    Rust fmt

--- components/nimbus/tests/test_message_helpers.rs
+++ components/nimbus/tests/test_message_helpers.rs
@@ -10,8 +10,6 @@ mod common;
 #[macro_use]
 use nimbus::error::Result;
 
-
-
 #[cfg(feature = "rkv-safe-mode")]
 #[cfg(test)]
 mod message_tests {
@@ -23,47 +21,44 @@ mod message_tests {
 
     #[test]
     fn test_jexl_expression() -> Result<()> {
-
         let nimbus = common::new_test_client("jexl_test")?;
         nimbus.initialize()?;
 
         let helper = nimbus.create_targeting_helper();
 
         // We get a boolean back from a string!
-        assert!(
-            helper.eval_jexl("app_name == 'fenix'".to_string(), None)?
-        );
+        assert!(helper.eval_jexl("app_name == 'fenix'".to_string(), None)?);
 
         // We get true and false back from two similar JEXL expressions!
         // I think we can convince ourselves that JEXL is being evaluated against the
         // AppContext.
-        assert!(
-            !helper.eval_jexl("app_name == 'xinef'".to_string(), None)?
-        );
+        assert!(!helper.eval_jexl("app_name == 'xinef'".to_string(), None)?);
 
         // The expression contains a variable not declared (snek_case Good, camelCase Bad)
-        assert!(
-            helper.eval_jexl("appName == 'fenix'".to_string(), None).is_err()
-        );
+        assert!(helper
+            .eval_jexl("appName == 'fenix'".to_string(), None)
+            .is_err());
 
         // Check the versionCompare function, just to prove to ourselves that it's the same JEXL evaluator.
-        assert!(
-            helper.eval_jexl(
-                "(version|versionCompare('95.!') >= 0) && (version|versionCompare('96.!') < 0)".to_string(),
-                json!({
-                    "version": "95.1.0".to_string(),
-                }).as_object().cloned(),
-            )?
-        );
+        assert!(helper.eval_jexl(
+            "(version|versionCompare('95.!') >= 0) && (version|versionCompare('96.!') < 0)"
+                .to_string(),
+            json!({
+                "version": "95.1.0".to_string(),
+            })
+            .as_object()
+            .cloned(),
+        )?);
 
-        assert!(
-            !helper.eval_jexl(
-                "(version|versionCompare('95.!') >= 0) && (version|versionCompare('96.!') < 0)".to_string(),
-                json!({
-                    "version": "94.0.0".to_string(),
-                }).as_object().cloned(),
-            )?
-        );
+        assert!(!helper.eval_jexl(
+            "(version|versionCompare('95.!') >= 0) && (version|versionCompare('96.!') < 0)"
+                .to_string(),
+            json!({
+                "version": "94.0.0".to_string(),
+            })
+            .as_object()
+            .cloned(),
+        )?);
 
         Ok(())
     }
@@ -75,13 +70,9 @@ mod message_tests {
 
         let helper = nimbus.create_targeting_helper();
 
-        assert!(
-            helper.eval_jexl("days_since_install == 0".to_string(), None)?
-        );
+        assert!(helper.eval_jexl("days_since_install == 0".to_string(), None)?);
 
-        assert!(
-            helper.eval_jexl("days_since_update == 0".to_string(), None)?
-        );
+        assert!(helper.eval_jexl("days_since_update == 0".to_string(), None)?);
 
         let app_context = AppContext {
             app_name: "fenix".to_string(),
@@ -100,13 +91,9 @@ mod message_tests {
         nimbus.with_targeting_attributes(targeting_attributes);
 
         let helper = nimbus.create_targeting_helper();
-        assert!(
-            helper.eval_jexl("days_since_install == 10".to_string(), None)?
-        );
+        assert!(helper.eval_jexl("days_since_install == 10".to_string(), None)?);
 
-        assert!(
-            helper.eval_jexl("days_since_update == 5".to_string(), None)?
-        );
+        assert!(helper.eval_jexl("days_since_update == 5".to_string(), None)?);
 
         Ok(())
     }
