c2ae3a5852f4a40a1457caae37c98554825da59e
Elise Richards <erichards@mozilla.com>
2022-02-16 11:47:42 -0800

    For EXP-2012: Move tests for defaults into their own file

--- components/nimbus/src/tests/mod.rs
+++ components/nimbus/src/tests/mod.rs
@@ -2,6 +2,7 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+mod test_defaults;
 mod test_enrollment;
 mod test_enrollment_bw_compat;
 mod test_evaluator;
