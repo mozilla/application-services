c60ac7bc53cb3584ab6f4b58b035a86430804b51
Elise Richards <erichards@mozilla.com>
2022-03-09 18:14:57 -0800

    Add allow tag to let test run in safe mode

--- components/nimbus/src/tests/client/test_null_client.rs
+++ components/nimbus/src/tests/client/test_null_client.rs
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+ // Allow this to run in "safe mode"
+#![allow(unused_imports)]
+
 use crate::error::Result;
 
 #[cfg(feature = "rkv-safe-mode")]
