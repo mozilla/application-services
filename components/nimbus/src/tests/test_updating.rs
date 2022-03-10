d1c175135dfe6e4a7e3c343d00c93193f47736c1
Elise Richards <erichards@mozilla.com>
2022-03-09 18:52:03 -0800

    For EXP-2012: Move tests for updating.rs into their own file (#4851)
    * For EXP-2012: Move tests for updating into their own file
    
    * Add allow tag to let tests run in safe mode

---
+++ components/nimbus/src/tests/test_updating.rs
@@ -0,0 +1,47 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Allow this to run in "safe mode"
+#![allow(unused_imports)]
+
+use crate::error::Result;
+use crate::updating::*;
+use crate::Database;
+use crate::Experiment;
+use tempdir::TempDir;
+
+// This test crashes lmdb for reasons that make no sense, so only run it
+// in the "safe mode" backend.
+#[cfg(feature = "rkv-safe-mode")]
+#[test]
+fn test_reading_writing_and_removing_experiments() -> Result<()> {
+    let tmp_dir = TempDir::new("test_stash_pop_updates")?;
+    let db = Database::new(&tmp_dir)?;
+    let mut writer = db.write()?;
+
+    let _ = env_logger::try_init();
+
+    let test_experiment: Experiment = Default::default();
+    let fetched = vec![test_experiment];
+
+    // simulated fetch by constructing a dummy payload of 1 experiment.
+    assert_eq!(fetched.len(), 1);
+
+    write_pending_experiments(&db, &mut writer, fetched)?;
+
+    // Now, we come to get the stashed updates, and they should be
+    // the same.
+    let pending = read_and_remove_pending_experiments(&db, &mut writer)?;
+
+    assert_eq!(pending.unwrap().len(), 1);
+
+    // After we've fetched this once, we should have no pending
+    // updates left.
+    let pending = read_and_remove_pending_experiments(&db, &mut writer)?;
+
+    assert!(pending.is_none(), "No pending updates should be stashed");
+
+    writer.commit()?;
+    Ok(())
+}
