b1ce6cc14a42cb7488e43de2e6e23f6e590379ef
James Hugman <james@hugman.tv>
2022-03-29 10:48:58 +0100

    Tidy templates into macros

--- components/support/nimbus-fml/src/backends/swift/templates/ObjectTemplate.swift
+++ components/support/nimbus-fml/src/backends/swift/templates/ObjectTemplate.swift
@@ -1,54 +1,15 @@
 {%- import "macros.swift" as swift %}
 {%- let inner = self.inner() %}
-{%- let raw_name = inner.name() %}
+{% call swift::render_class(inner) -%}
 {% let class_name = inner.name()|class_name -%}
 
-{{ inner.doc()|comment("") }}
-public class {{class_name}} {
-    private var _variables: Variables
-    var _defaults: Defaults
-    init(variables: Variables, defaults: Defaults) {
-        self._variables = variables
-        self._defaults = defaults
-    }
-    {# The struct holds the default values that come from the manifest. They should completely
-    specify all values needed for the  feature #}
-    struct Defaults {
-        {% for p in inner.props() %}
-        {%- let t = p.typ() %}
-        let {{p.name()|var_name}}: {{ t|defaults_type_label }}
-    {%- endfor %}
-    }
-
-    {#- A constructor for application tests to use.  #}
-
-    public convenience init(
-        _variables: Variables = NilVariables.instance, {% for p in inner.props() %}
-        {%- let t = p.typ() %}
-        {{p.name()|var_name}}: {{ t|defaults_type_label }} = {{ t|literal(self, p.default(), "") }}{% if !loop.last %},{% endif %}
-    {%- endfor %}
-    ) {
-        self.init(variables: _variables, defaults: Defaults({% for p in inner.props() %}
-        {%- let nm = p.name()|var_name %}{{ nm }}: {{ nm }}{% if !loop.last %}, {% endif %}
-        {%- endfor %}))
-    }
-
-{#- The property initializers #}
-{# -#}
-    {% for p in inner.props() %}
-    {%- let prop_swift = p.name()|var_name %}
-    {{ p.doc()|comment("    ") }}
-    public lazy var {{ prop_swift }}: {{ p.typ()|type_label }} = {
-        {%- let defaults = format!("_defaults.{}", prop_swift) %}
-        {{ p.typ()|property(p.name(), "_variables", defaults)}}
-    }()
-    {%- endfor %}
-
+public extension {{class_name}} {
     func _mergeWith(_ defaults: {{class_name}}) -> {{class_name}} {
         return {{class_name}}(variables: self._variables, defaults: defaults._defaults)
     }
+
     static func create(_ variables: Variables?) -> {{class_name}} {
-        return {{class_name}}(_variables: variables ?? NilVariables.instance)
+        return {{class_name}}(variables ?? NilVariables.instance)
     }
 
     static func mergeWith(_ overrides: {{class_name}}, _ defaults: {{class_name}}) -> {{class_name}} {
--- components/support/nimbus-fml/src/backends/swift/templates/macros.swift
+++ components/support/nimbus-fml/src/backends/swift/templates/macros.swift
@@ -6,4 +6,52 @@
 
     It is the natural place to put commonalities between Object and Features,
     and rendering literals for Objects.
-    -#}
+-#}
+
+{% macro render_class(inner) %}
+{%- let raw_name = inner.name() %}
+{% let class_name = inner.name()|class_name -%}
+
+{{ inner.doc()|comment("") }}
+public class {{class_name}} {
+    private let _variables: Variables
+    private let _defaults: Defaults
+    private init(variables: Variables = NilVariables.instance, defaults: Defaults) {
+        self._variables = variables
+        self._defaults = defaults
+    }
+    {# The struct holds the default values that come from the manifest. They should completely
+    specify all values needed for the  feature #}
+    struct Defaults {
+        {%- for p in inner.props() %}
+        {%- let t = p.typ() %}
+        let {{p.name()|var_name}}: {{ t|defaults_type_label }}
+    {%- endfor %}
+    }
+
+    {#- A constructor for application tests to use.  #}
+
+    public convenience init(
+        _ _variables: Variables = NilVariables.instance, {% for p in inner.props() %}
+        {%- let t = p.typ() %}
+        {{p.name()|var_name}}: {{ t|defaults_type_label }} = {{ t|literal(self, p.default(), "") }}{% if !loop.last %},{% endif %}
+    {%- endfor %}
+    ) {
+        self.init(variables: _variables, defaults: Defaults({% for p in inner.props() %}
+            {{p.name()|var_name}}: {{p.name()|var_name}}{% if !loop.last %},{% endif %}
+        {%- endfor %}))
+    }
+
+{#- The property initializers #}
+{# -#}
+    {% for p in inner.props() %}
+    {%- let prop_swift = p.name()|var_name %}
+    {{ p.doc()|comment("    ") }}
+    public lazy var {{ prop_swift }}: {{ p.typ()|type_label }} = {
+        {%- let defaults = format!("_defaults.{}", prop_swift) %}
+        {{ p.typ()|property(p.name(), "self._variables", defaults)}}
+    }()
+    {%- endfor %}
+}
+
+{% endmacro %}}
