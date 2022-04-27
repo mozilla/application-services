0a5fa1ee7515828edd1acc793b76a774806a8210
Tarik Eshaq <teshaq@mozilla.com>
2022-04-27 14:39:59 -0700

    Makes GetSdk public for FeatureHolder

--- components/nimbus/ios/Nimbus/FeatureHolder.swift
+++ components/nimbus/ios/Nimbus/FeatureHolder.swift
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-typealias GetSdk = () -> FeaturesInterface?
+public typealias GetSdk = () -> FeaturesInterface?
 
 public class FeatureHolder<T> {
     private let getSdk: GetSdk
