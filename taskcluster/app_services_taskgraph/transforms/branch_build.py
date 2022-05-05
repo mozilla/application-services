ced0518e9fc189ccea3e8d30e4fc101be44a3f82
bendk <bdk@mozilla.com>
2022-05-05 10:13:46 -0400

    Fixes for nighly builds (#4938)
    Added a `route` to our nightly branch build tasks.  I think this should
    mean we can view the latest branch builds via a taskcluster index.
    
    Moved the code to calculate if we're in a nightly into the top-level
    `get_decision_parameters()` function.  It seems good to put this in one
    place, especially since the way we determine if we're in a nightly build
    is pretty hacky.

--- taskcluster/app_services_taskgraph/transforms/branch_build.py
+++ taskcluster/app_services_taskgraph/transforms/branch_build.py
@@ -8,6 +8,8 @@ from taskgraph.transforms.base import TransformSequence
 from taskgraph.util.schema import validate_schema, Schema
 from voluptuous import Optional, Required, In
 
+NIGHLY_INDEX = "index.project.application-services.v2.nightlies"
+
 # Schema for the job dictionary from kinds.yml
 branch_build_schema = Schema({
   # Which repository are we working on
@@ -94,6 +96,7 @@ def setup(config, tasks):
         else:
             raise ValueError("Invalid branch build operation: {}".format(operation))
         task['description'] = '{} {}'.format(operation, repo_name)
+        setup_routes(task, config.params)
         yield task
 
 def setup_application_services(task):
@@ -174,3 +177,7 @@ def setup_fenix_build(task):
 
 def setup_test(task, repo_name):
     task['run']['gradlew'] = ['testDebugUnitTest']
+
+def setup_routes(task, params):
+    if params.get('nighly-build'):
+        task.setdefault('routes', []).append(NIGHLY_INDEX)
