7ffcf2c771dacad9da4494e56f45099dbe342879
Tarik Eshaq <teshaq@mozilla.com>
2022-05-08 21:45:40 -0700

    Fix rustfmt

--- megazords/ios-rust/focus/src/lib.rs
+++ megazords/ios-rust/focus/src/lib.rs
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- #![allow(unknown_lints)]
- #![warn(rust_2018_idioms)]
- 
- pub use nimbus;
- pub use rc_log_ffi;
- pub use viaduct_reqwest;
+#![allow(unknown_lints)]
+#![warn(rust_2018_idioms)]
+
+pub use nimbus;
+pub use rc_log_ffi;
+pub use viaduct_reqwest;
