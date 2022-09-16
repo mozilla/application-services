45380b17a81762a1c9e8358f0366461635bbc678
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2022-09-15 23:10:15 -0400

    Fix or suppress new detekt errors.

--- components/tabs/android/src/main/java/mozilla/appservices/tabs/RemoteTab.kt
+++ components/tabs/android/src/main/java/mozilla/appservices/tabs/RemoteTab.kt
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+@file:Suppress("InvalidPackageDeclaration")
 package mozilla.appservices.remotetabs
 
 // We needed to rename the Rust `RemoteTab` struct to `RemoteTabRecord` in order to circumvent the naming conflict in
