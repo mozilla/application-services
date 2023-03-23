6bf0ac92bf28f2e4fc55c5a77252c345c493d063
James Hugman <james@hugman.tv>
2023-03-23 22:14:54 +0000

    Add Kotlin to record exposure and malformed configuration

--- components/nimbus/android/src/main/java/org/mozilla/experiments/nimbus/NimbusInterface.kt
+++ components/nimbus/android/src/main/java/org/mozilla/experiments/nimbus/NimbusInterface.kt
@@ -158,34 +158,6 @@ interface NimbusInterface : FeaturesInterface, GleanPlumbInterface, NimbusEventS
     fun resetTelemetryIdentifiers() = Unit
 
     /**
-     * Records the `exposure` event in telemetry.
-     *
-     * This is a manual function to accomplish the same purpose as passing `true` as the
-     * `recordExposureEvent` property of the [getVariables] function. It is intended to be used
-     * when requesting feature variables must occur at a different time than the actual user's
-     * exposure to the feature within the app.
-     *
-     * Examples:
-     * * If the [Variables] are needed at a different time than when the exposure to the feature
-     *   actually happens, such as constructing a menu happening at a different time than the user
-     *   seeing the menu.
-     * * If [getVariables] is required to be called multiple times for the same feature and it is
-     *   desired to only record the exposure once, such as if [getVariables] were called with every
-     *   keystroke.
-     *
-     * In the case where the use of this function is required, then the [getVariables] function
-     * should be called with `false` so that the exposure event is not recorded when the variables
-     * are fetched.
-     *
-     * This function is safe to call even when there is no active experiment for the feature. The SDK
-     * will ensure that an event is only recorded for active experiments.
-     *
-     * @param featureId string representing the id of the feature for which to record the exposure
-     *     event.
-     */
-    override fun recordExposureEvent(featureId: String) = Unit
-
-    /**
      * Control the opt out for all experiments at once. This is likely a user action.
      */
     var globalUserParticipation: Boolean
