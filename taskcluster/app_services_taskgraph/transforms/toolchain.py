262668d547495f57f57086aeb8bbeb3c4beb1c25
Julien Cristau <jcristau@mozilla.com>
2023-04-27 15:50:57 +0200

    ci: don't point toolchain indices at pull-request tasks
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1830149

--- taskcluster/app_services_taskgraph/transforms/toolchain.py
+++ taskcluster/app_services_taskgraph/transforms/toolchain.py
@@ -35,7 +35,7 @@ def resolve_keys(config, tasks):
         })
         # TODO: Bug 1637695 - temp solution to unblock local building of
         # appplication-services. Once we switch to new indexes, we should clean this up
-        if task['name'] in TOOLCHAIN_OLD_INDEX.keys():
+        if task['name'] in TOOLCHAIN_OLD_INDEX.keys() and config.params["level"] == "3":
             sha = git_sha_for_directory("libs")
             routes = task['routes']
             routes.append(TOOLCHAIN_OLD_INDEX[task['name']].format(sha=sha))
