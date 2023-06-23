2bf9814eddd5bc8f8b86ea93863df7efcf92601f
Ben Dean-Kawamura <bdk@mozilla.com>
2023-06-23 12:49:23 -0400

    Added compatability methods to `FxaConfig` and related types
    The latest Fenix nightly is failing because it was using the
    Rust versions of FxaConfig and FxaServer.  Adding some methods to
    prevent the breakage

--- components/fxa-client/android/src/main/java/mozilla/appservices/fxaclient/Config.kt
+++ components/fxa-client/android/src/main/java/mozilla/appservices/fxaclient/Config.kt
@@ -20,6 +20,9 @@ class Config constructor(
         STAGE(FxaServer.Stage),
         CHINA(FxaServer.China),
         LOCALDEV(FxaServer.LocalDev),
+        ;
+
+        val contentUrl get() = this.rustServer.contentUrl
     }
 
     constructor(
@@ -29,8 +32,27 @@ class Config constructor(
         tokenServerUrlOverride: String? = null,
     ) : this(server.rustServer, clientId, redirectUri, tokenServerUrlOverride)
 
+    constructor(
+        contentUrl: String,
+        clientId: String,
+        redirectUri: String,
+        tokenServerUrlOverride: String? = null,
+    ) : this(FxaServer.Custom(contentUrl), clientId, redirectUri, tokenServerUrlOverride)
+
+    val contentUrl get() = this.server.contentUrl
+
     // Rust defines a config and server class that's virtually identically to these.  We should
     // remove the wrapper soon, but let's wait until we have a batch of breaking changes and do them
     // all at once.
     fun intoRustConfig() = FxaConfig(server, clientId, redirectUri, tokenServerUrlOverride)
 }
+
+val FxaServer.contentUrl: String
+    get() = when (this) {
+        is FxaServer.Release -> "https://accounts.firefox.com"
+        is FxaServer.Stable -> "https://stable.dev.lcip.org"
+        is FxaServer.Stage -> "https://accounts.stage.mozaws.net"
+        is FxaServer.China -> "https://accounts.firefox.com.cn"
+        is FxaServer.LocalDev -> "http://127.0.0.1:3030"
+        is FxaServer.Custom -> this.url
+    }
