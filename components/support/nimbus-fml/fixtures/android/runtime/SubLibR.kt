8f6b9612cd0e6dd07275d2a6b955478ae967c4d3
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-06-29 23:32:47 -0400

    Update ktlint to version 0.50.0 and fix new warnings

--- components/support/nimbus-fml/fixtures/android/runtime/SubLibR.kt
+++ components/support/nimbus-fml/fixtures/android/runtime/SubLibR.kt
@@ -1,9 +1,8 @@
-// ktlint-disable filename
 /* This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-@file:Suppress("ClassName", "InvalidPackageDeclaration", "MatchingDeclarationName")
+@file:Suppress("ClassName", "InvalidPackageDeclaration", "ktlint:standard:filename", "MatchingDeclarationName")
 
 package com.example.sublib
 /**
