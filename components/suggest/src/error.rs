c4a4a96de5a176df6f4ba6512f8626285457cf5b
Lina Butler <lina@yakshaving.ninja>
2023-07-14 15:47:06 -0700

    suggest: Add MPL license headers.

--- components/suggest/src/error.rs
+++ components/suggest/src/error.rs
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 /// A list of errors that are internal to the component. This is the error
 /// type for private and crate-internal methods, and is never returned to the
 /// application.
--- components/suggest/src/keyword.rs
+++ components/suggest/src/keyword.rs
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 /// Given a list of keywords for a suggestion, returns a phrase that best
 /// completes the user's query. This function uses two heuristics to pick the
 /// best match:
