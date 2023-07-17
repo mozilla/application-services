102499cb59c8673cef83b0c030692d83e52d78dd
James Hugman <james@hugman.tv>
2023-07-17 20:03:23 +0100

    Add filters to lists

--- components/support/nimbus-cli/src/sources/mod.rs
+++ components/support/nimbus-cli/src/sources/mod.rs
@@ -4,11 +4,10 @@
 
 mod experiment;
 mod experiment_list;
+mod filter;
 mod manifest;
 
 pub(crate) use experiment::ExperimentSource;
 pub(crate) use experiment_list::ExperimentListSource;
+pub(crate) use filter::ExperimentListFilter;
 pub(crate) use manifest::ManifestSource;
-
-#[cfg(test)]
-pub(crate) use experiment_list::ExperimentListFilter;
