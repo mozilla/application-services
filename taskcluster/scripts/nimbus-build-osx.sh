1a69fce054fb80a4b1302b7e5001312c6586c60b
Michelle Goossens <michelle@masterwayz.nl>
2023-07-27 17:00:41 +0200

    Switch to macOS 1333 SDK

--- taskcluster/scripts/nimbus-build-osx.sh
+++ taskcluster/scripts/nimbus-build-osx.sh
@@ -5,7 +5,7 @@ set -ex
 # This runs in front of `build-nimbus-fml.py`  The only reason it exists is that it's easier to
 # setup the enviroment in a script.
 
-SDK=macosx11.0
+SDK=macosx13.3
 xcodebuild -showsdks
 SDKROOT=$(xcrun -sdk $SDK --show-sdk-path)
 MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk $SDK --show-sdk-platform-version)
