294808adf5eb48bdbcee06f818bc33f6c5739374
Charlie <chumphreys@mozilla.com>
2023-09-06 16:30:33 -0500

    update license urls to be https

--- components/nimbus/src/stateful/client/http_client.rs
+++ components/nimbus/src/stateful/client/http_client.rs
@@ -1,6 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 //! This is a simple HTTP client that uses viaduct to retrieve experiment data from the server.
 //! Currently configured to use Kinto and the old schema, although that would change once we start
