f9f3703ad11e809de4ce56e882ed190e7748bbf7
Barret Rennie <barret@brennie.ca>
2023-09-20 14:29:15 -0400

    Build linux nimbus binaries with Ubuntu 20.04
    This introduces a second docker image based on Ubuntu 20.04 to build
    nimbus-fml and nimbus-cli for the x86_64-unknown-linux-gnu and
    aarch64-unknown-linux-gnu targets. The regular container uses a newer
    version of Ubuntu (22.04) which has a newer version of glibc, which
    makes the binaries harder to distribute. Building with an older version
    of glibc means that the *-unknown-linux-gnu binaries can be used by
    @mozilla/experimenter.

--- taskcluster/scripts/toolchain/build-rust-toolchain.sh
+++ taskcluster/scripts/toolchain/build-rust-toolchain.sh
@@ -30,6 +30,7 @@ rustup --version
 rustup target add x86_64-apple-darwin
 rustup target add x86_64-pc-windows-gnu
 rustup target add x86_64-unknown-linux-musl
+rustup target add aarch64-unknown-linux-gnu
 rustup target add aarch64-unknown-linux-musl
 
 # Tar everything into UPLOAD_DIR
