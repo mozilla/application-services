ecef2d4444b3c2309e41bdc48e2c21b1e39c53b8
James Hugman <james@hugman.tv>
2023-10-23 12:14:11 +0100

    Address reviewer comments

--- components/nimbus/ios/scripts/nimbus-fml-configuration.sh
+++ components/nimbus/ios/scripts/nimbus-fml-configuration.sh
@@ -4,6 +4,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+set -euo pipefail
+
 # Local modifications/additions can be made in `nimbus-fml-configuration.local.sh`
 
 ## Set the channel that is used to generate the Swift code.
