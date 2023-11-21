9226b356aa00d96c570fa1100ec0ce6b584ea3d0
James Hugman <james@hugman.tv>
2023-11-21 19:28:37 +0000

    Add sha2 hasher for structure-hasher and defaults-hasher

--- components/support/nimbus-fml/src/structure/mod.rs
+++ components/support/nimbus-fml/src/structure/mod.rs
@@ -2,6 +2,7 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+mod hasher;
 mod validator;
 
 pub(crate) use validator::StructureValidator;
