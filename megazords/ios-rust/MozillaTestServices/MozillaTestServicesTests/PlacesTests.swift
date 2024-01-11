36b7d68d242dd792c4b9f30936ce23cf128170d1
Ben Dean-Kawamura <bdk@mozilla.com>
2024-01-11 09:59:16 -0500

    Handful of swiftformat fixes

--- megazords/ios-rust/MozillaTestServices/MozillaTestServicesTests/PlacesTests.swift
+++ megazords/ios-rust/MozillaTestServices/MozillaTestServicesTests/PlacesTests.swift
@@ -1,8 +1,10 @@
 @testable import MozillaTestServices
+
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 import XCTest
+
 // some utility functions for the test code
 
 func dynCmp<T: Equatable>(_ optVal: T?, _ optDynVal: Any?) -> Bool {
