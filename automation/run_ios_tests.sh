7c81f70d8a8f216d965b57e439d4c56d25036420
Tarik Eshaq <teshaq@mozilla.com>
2024-04-24 11:22:57 -0400

    Uses m1 runners for circle ci because x86 is deprecated

--- automation/run_ios_tests.sh
+++ automation/run_ios_tests.sh
@@ -5,10 +5,10 @@ set -euvx
 ./megazords/ios-rust/build-xcframework.sh --build-profile release
 set -o pipefail && \
 xcodebuild \
-  -workspace ./megazords/ios-rust/MozillaTestServices/MozillaTestServices.xcodeproj/project.xcworkspace \
+  -workspace megazords/ios-rust/MozillaTestServices/MozillaTestServices.xcodeproj/project.xcworkspace \
   -scheme MozillaTestServices \
   -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPhone 14' \
+  -destination 'platform=iOS Simulator,name=iPhone 15' \
   test | \
 tee raw_xcodetest.log | \
 xcpretty && exit "${PIPESTATUS[0]}"
