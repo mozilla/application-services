mozilla/application-services

commit c6fdae855cb2069b8326879bf18902c976a78159

fxa_client: Specify a 48-hour TTL for the "close tabs" command.

components/fxa-client/src/internal/http_client.rs
@@ -123,6 +123,7 @@ pub(crate) trait FxAClient {
         command: &str,
         target: &str,
         payload: &serde_json::Value,
+        ttl: Option<u64>,
     ) -> Result<()>;
     fn update_device_record<'a>(
         &self,
@@ -359,11 +360,13 @@ impl FxAClient for Client {
         command: &str,
         target: &str,
         payload: &serde_json::Value,
+        ttl: Option<u64>,
     ) -> Result<()> {
         let body = json!({
             "command": command,
             "target": target,
-            "payload": payload
+            "payload": payload,
+            "ttl": ttl,
         });
         let url = config.auth_url_path("v1/account/devices/invoke_command")?;
         let request = Request::post(url)

components/fxa-client/src/internal/close_tabs.rs
@@ -25,7 +25,12 @@ impl FirefoxAccount {
         };
         let oldsync_key = self.get_scoped_key(scopes::OLD_SYNC)?;
         let command_payload = close_tabs::build_close_tabs_command(oldsync_key, target, &payload)?;
-        self.invoke_command(close_tabs::COMMAND_NAME, target, &command_payload)?;
+        self.invoke_command(
+            close_tabs::COMMAND_NAME,
+            target,
+            &command_payload,
+            Some(close_tabs::COMMAND_TTL),
+        )?;
         Ok(())
     }
 

components/fxa-client/src/internal/commands/close_tabs.rs
@@ -14,6 +14,7 @@ use super::{
 };
 
 pub const COMMAND_NAME: &str = "https://identity.mozilla.com/cmd/close-uri/v1";
+pub const COMMAND_TTL: u64 = 2 * 24 * 3600;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct EncryptedCloseTabsPayload {

components/fxa-client/src/internal/device.rs
@@ -159,6 +159,7 @@ impl FirefoxAccount {
         command: &str,
         target: &Device,
         payload: &serde_json::Value,
+        ttl: Option<u64>,
     ) -> Result<()> {
         let refresh_token = self.get_refresh_token()?;
         self.client.invoke_command(
@@ -167,6 +168,7 @@ impl FirefoxAccount {
             command,
             &target.id,
             payload,
+            ttl,
         )
     }
 

components/fxa-client/src/internal/send_tab.rs
@@ -53,7 +53,7 @@ impl FirefoxAccount {
         let (payload, sent_telemetry) = SendTabPayload::single_tab(title, url);
         let oldsync_key = self.get_scoped_key(scopes::OLD_SYNC)?;
         let command_payload = send_tab::build_send_command(oldsync_key, target, &payload)?;
-        self.invoke_command(send_tab::COMMAND_NAME, target, &command_payload)?;
+        self.invoke_command(send_tab::COMMAND_NAME, target, &command_payload, None)?;
         self.telemetry.record_command_sent(sent_telemetry);
         Ok(())
     }

commit ce4f33366d8e69f816903579f06d2186f07204b5

fxa-client: Omit `ttl` fields from device commands if not set.
c6fdae855cb2069b8326879bf18902c976a78159 caused Send Tab commands to
be sent with `"ttl": "null"` to the FxA server, since Send Tab
doesn't specify an explicit TTL.

This fixes bug 1895711.

components/fxa-client/src/internal/http_client.rs
@@ -362,17 +362,17 @@ impl FxAClient for Client {
         payload: &serde_json::Value,
         ttl: Option<u64>,
     ) -> Result<()> {
-        let body = json!({
-            "command": command,
-            "target": target,
-            "payload": payload,
-            "ttl": ttl,
-        });
+        let body = serde_json::to_string(&InvokeCommandRequest {
+            command,
+            target,
+            payload,
+            ttl,
+        })?;
         let url = config.auth_url_path("v1/account/devices/invoke_command")?;
         let request = Request::post(url)
             .header(header_names::AUTHORIZATION, bearer_token(refresh_token))?
             .header(header_names::CONTENT_TYPE, "application/json")?
-            .body(body.to_string());
+            .body(body);
         self.make_request(request)?;
         Ok(())
     }
@@ -955,6 +955,15 @@ pub struct DuplicateTokenResponse {
     pub auth_at: u64,
 }
 
+#[derive(Serialize)]
+struct InvokeCommandRequest<'a> {
+    pub command: &'a str,
+    pub target: &'a str,
+    pub payload: &'a serde_json::Value,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ttl: Option<u64>,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -977,6 +986,50 @@ mod tests {
             ttl: Some(123),
         };
         assert_eq!("{\"grant_type\":\"refresh_token\",\"client_id\":\"bar\",\"refresh_token\":\"foo\",\"scope\":\"bobo\",\"ttl\":123}", serde_json::to_string(&using_code).unwrap());
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn check_InvokeCommandRequest_serialization() -> Result<()> {
+        let payload = json!({
+            "a": "b",
+        });
+        let with_ttl = InvokeCommandRequest {
+            command: "with_ttl",
+            target: "a",
+            payload: &payload,
+            ttl: Some(30),
+        };
+        assert_eq!(
+            serde_json::to_value(with_ttl)?,
+            json!({
+                "command": "with_ttl",
+                "target": "a",
+                "payload": {
+                    "a": "b",
+                },
+                "ttl": 30,
+            })
+        );
+
+        let without_ttl = InvokeCommandRequest {
+            command: "without_ttl",
+            target: "b",
+            payload: &payload,
+            ttl: None,
+        };
+        assert_eq!(
+            serde_json::to_value(without_ttl)?,
+            json!({
+                "command": "without_ttl",
+                "target": "b",
+                "payload": {
+                    "a": "b",
+                },
+            })
+        );
+
+        Ok(())
     }
 
     #[test]