cfb19af8ee9d5eb35ef93bba0eaf4949ea5fba96
Ben Dean-Kawamura <bdk@mozilla.com>
2024-08-15 14:46:28 -0400

    Bug 1889982 - Upload android symbols
    - Split the `upload_android_symbols.sh` into a generation script and an
      upload script.
    - `module-build` tasks that have `uploadSymbols` set run the generation script.
    - Added the `upload-symbols` task that to run the upload script.  This
      runs during the `ship` phase, the nightly cron job, and also for `[ci full]`
      (the latter uses dummy credentials).

--- automation/upload_android_symbols.sh
+++ automation/upload_android_symbols.sh
@@ -5,7 +5,7 @@ set -euvx
 if [[ "${#}" -ne 1 ]]
 then
     echo "Usage:"
-    echo "./automation/upload_android_symbols.sh <project path>"
+    echo "./automation/upload_android_symbols.sh <symbols dir>"
     exit 1
 fi
 
@@ -15,39 +15,7 @@ then
     exit 1
 fi
 
-PROJECT_PATH=${1}
+SYMBOLS_DIR=${1}
 
-pushd libs
-# shellcheck disable=SC1091
-source "android_defaults.sh"
-popd
-
-OUTPUT_FOLDER="crashreporter-symbols"
-DUMP_SYMS_DIR="automation/symbols-generation/bin"
-
-if [[ ! -f "${DUMP_SYMS_DIR}/dump_syms" ]]; then
-  tooltool.py --manifest=automation/symbols-generation/dump_syms.manifest --url=http://taskcluster/tooltool.mozilla-releng.net/ fetch
-  chmod +x dump_syms
-  mkdir -p "${DUMP_SYMS_DIR}"
-  mv dump_syms "${DUMP_SYMS_DIR}"
-fi
-
-# Keep the 3 in sync.
-TARGET_ARCHS=("x86_64" "x86" "arm64" "arm")
-JNI_LIBS_TARGETS=("x86_64" "x86" "arm64-v8a" "armeabi-v7a")
-
-rm -rf "${OUTPUT_FOLDER}"
-mkdir -p "${OUTPUT_FOLDER}"
-
-# 1. Generate the symbols.
-for i in "${!TARGET_ARCHS[@]}"; do
-  export OBJCOPY="${ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt/${NDK_HOST_TAG}/bin/llvm-objcopy"
-  JNI_SO_PATH="${PROJECT_PATH}/build/rustJniLibs/android/${JNI_LIBS_TARGETS[${i}]}"
-  for sofile in "${JNI_SO_PATH}"/*.so; do
-    python3 automation/symbols-generation/symbolstore.py -c -s . --vcs-info "${DUMP_SYMS_DIR}"/dump_syms "${OUTPUT_FOLDER}" "${sofile}"
-  done
-done
-
-# 2. Upload them.
 pip3 install --user -r automation/symbols-generation/requirements.txt
-python3 automation/symbols-generation/upload_symbols.py "${OUTPUT_FOLDER}" -t "$PWD/.symbols_upload_token"
+python3 automation/symbols-generation/upload_symbols.py "${SYMBOLS_DIR}" -t "$PWD/.symbols_upload_token"
