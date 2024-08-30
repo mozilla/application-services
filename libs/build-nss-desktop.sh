4e5c7e3d036bc2ea8a96aa1028019ac1d8f04879
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2024-08-30 08:50:00 -0400

    Update NSS to version 3.104

--- libs/build-nss-desktop.sh
+++ libs/build-nss-desktop.sh
@@ -63,7 +63,7 @@ if [[ "${CROSS_COMPILE_TARGET}" =~ "darwin" ]]; then
   else
     # From https://firefox-ci-tc.services.mozilla.com/tasks/index/app-services.cache.level-3.content.v1.nss-artifact/latest
     curl -sfSL --retry 5 --retry-delay 10 -O "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/app-services.cache.level-3.content.v1.nss-artifact.latest/artifacts/public%2Fdist.tar.bz2"
-    SHA256="c08c4803f72286d5f88fea3c83d1b03b866acae1c433c8278a812c6c133091c7"
+    SHA256="585c55b69313168fbddd54f17b1c5ccfbfb2e30a2088bb00c56b2d7f3b7110cc"
     echo "${SHA256}  dist.tar.bz2" | shasum -a 256 -c - || exit 2
     tar xvjf dist.tar.bz2 && rm -rf dist.tar.bz2
     NSS_DIST_DIR=$(abspath "dist")
