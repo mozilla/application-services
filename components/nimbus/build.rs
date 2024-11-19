c348fba48ade24f39d23ac42e1a34c5f2dc894b7
Mark Hammond <mhammond@skippinet.com.au>
2024-11-19 10:19:20 -0500

    nimbus doesn't use/need glean-build

--- components/nimbus/build.rs
+++ components/nimbus/build.rs
@@ -2,19 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-use glean_build::Builder;
-
 pub fn main() {
     #[cfg(feature = "stateful-uniffi-bindings")]
     uniffi::generate_scaffolding("./src/nimbus.udl").unwrap();
 
     #[cfg(not(feature = "stateful-uniffi-bindings"))]
     uniffi::generate_scaffolding("./src/cirrus.udl").unwrap();
-
-    Builder::default()
-        .file("./metrics.yaml")
-        .generate()
-        .unwrap();
-
-    println!("cargo:rerun-if-changed=./metrics.yaml");
 }
