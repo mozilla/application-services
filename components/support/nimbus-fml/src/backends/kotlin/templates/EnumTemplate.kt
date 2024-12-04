27e6e08f03c5c1af1f67459aa272979f883e78cf
Mark Hammond <mhammond@skippinet.com.au>
2024-12-04 13:14:11 -0500

    Remove license headers from nimbus-fml templates

--- components/support/nimbus-fml/src/backends/kotlin/templates/EnumTemplate.kt
+++ components/support/nimbus-fml/src/backends/kotlin/templates/EnumTemplate.kt
@@ -1,7 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
 {% import "macros.kt" as kt %}
 {% let inner = self.inner() %}
 {% let class_name = inner.name()|class_name %}
