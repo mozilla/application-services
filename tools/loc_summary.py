2db9c35c08af5a407ab0aff876d0b651c45bcf35
Mark Banner <standard8@mozilla.com>
2024-12-03 21:27:44 -0500

    Add missing license headers

--- tools/loc_summary.py
+++ tools/loc_summary.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #
 # This is a simple script to generate some summary lines-of-code metrics.
 # Use it like this:
