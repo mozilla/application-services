b53ce521b1c1830b6247e6b48750ed7e293b7d8e
Mark Hammond <mhammond@skippinet.com.au>
2025-01-29 15:23:02 -0500

    Bump Rust to 1.84.0 to match mozilla-central

--- components/nimbus/src/tests/helpers.rs
+++ components/nimbus/src/tests/helpers.rs
@@ -2,6 +2,8 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#![allow(unexpected_cfgs)]
+
 use crate::{
     enrollment::{EnrolledFeatureConfig, EnrolledReason, ExperimentEnrollment, NotEnrolledReason},
     metrics::{EnrollmentStatusExtraDef, MetricsHandler},
