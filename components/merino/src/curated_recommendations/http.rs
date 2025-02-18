ec2a942419b1e56e388c569b73f14cb19374f540
Temisan Iwere <tiwere@mozilla.com>
2025-02-18 14:26:25 +0100

    [DISCO-3200] setup for curated rec client
    update error module
    
    add pub api library
    
    implement get error handling
    
    add tests

---
+++ components/merino/src/curated_recommendations/http.rs
@@ -0,0 +1,80 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use super::error::Error;
+use super::models::{CuratedRecommendationsRequest, CuratedRecommendationsResponse};
+use url::Url;
+use viaduct::{header_names, Request, Response};
+
+pub struct HttpClient {}
+
+impl HttpClient {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn make_curated_recommendation_request(
+        &self,
+        request: &CuratedRecommendationsRequest,
+        user_agent_header: &str,
+        base_host: &str, // e.g. "https://merino.services.mozilla.com"
+    ) -> Result<CuratedRecommendationsResponse, Error> {
+        let full_url = format!("{}/api/v1/curated-recommendations", base_host);
+        let url = Url::parse(&full_url)?;
+        log::trace!("making request: {url}");
+        let response: Response = Request::post(url)
+            .header(header_names::ACCEPT, "application/json")?
+            .header(header_names::USER_AGENT, user_agent_header)?
+            .json(request)
+            .send()?;
+
+        let status = response.status;
+        if status >= 400 {
+            let error_message = response.text();
+            let error = match status {
+                400 => Error::BadRequest {
+                    code: status,
+                    message: error_message.to_string(),
+                },
+                422 => Error::Validation {
+                    code: status,
+                    message: error_message.to_string(),
+                },
+                500..=599 => Error::Server {
+                    code: status,
+                    message: error_message.to_string(),
+                },
+                _ => Error::Unexpected {
+                    code: status,
+                    message: error_message.to_string(),
+                },
+            };
+            return Err(error);
+        }
+
+        let response_json: CuratedRecommendationsResponse = response.json()?;
+        log::trace!("response: {}", response.text());
+        Ok(response_json)
+    }
+}
+
+pub trait HttpClientTrait {
+    fn make_curated_recommendation_request(
+        &self,
+        request: &CuratedRecommendationsRequest,
+        user_agent_header: &str,
+        base_host: &str,
+    ) -> super::error::Result<CuratedRecommendationsResponse>;
+}
+
+impl HttpClientTrait for HttpClient {
+    fn make_curated_recommendation_request(
+        &self,
+        request: &CuratedRecommendationsRequest,
+        user_agent_header: &str,
+        base_host: &str,
+    ) -> super::error::Result<CuratedRecommendationsResponse> {
+        self.make_curated_recommendation_request(request, user_agent_header, base_host)
+    }
+}
