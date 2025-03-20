cfadc853efc680b0792208e7a01f0e463efa2ab8
Mark Hammond <mhammond@skippinet.com.au>
2025-03-20 16:10:32 -0400

    Fix linting issues reported in m-c

--- components/init_rust_components/src/lib.rs
+++ components/init_rust_components/src/lib.rs
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 uniffi::setup_scaffolding!();
 
 /// Initialization of the megazord crate. Must be called before any other calls to rust components.
