b1a6025e842205ce6f622bde3032f118d3f017f6
Mark Hammond <mhammond@skippinet.com.au>
2025-04-30 16:24:59 -0400

    More lint fixups from mozilla-central.
    I got some pushback from the linting team about excluding these from linting
    in m-c, so here we are.

--- components/support/nimbus-cli/assets/script.js
+++ components/support/nimbus-cli/assets/script.js
@@ -3,30 +3,31 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 function onReload() {
-    const uaString = window.navigator.userAgent.toLowerCase();
-    let el;
-    if (uaString.indexOf("iphone") >= 0) {
-        el = document.getElementById("ios-latest");
-    } else if (uaString.indexOf("android") >= 0) {
-        el = document.getElementById("android-latest");
-    } else {
-        el = document.getElementById("web-latest");
-    }
+  const uaString = window.navigator.userAgent.toLowerCase();
+  let el;
+  if (uaString.includes("iphone")) {
+    el = document.getElementById("ios-latest");
+  } else if (uaString.includes("android")) {
+    el = document.getElementById("android-latest");
+  } else {
+    el = document.getElementById("web-latest");
+  }
 
-    const button = document.getElementById("the-only-button");
-    if (el) {
-        button.textContent = String.fromCodePoint(0x25B6);
-        el.click()
-    } else {
-        button.textContent = String.fromCodePoint(0x1F504);
-    }
+  const button = document.getElementById("the-only-button");
+  if (el) {
+    button.textContent = String.fromCodePoint(0x25b6);
+    el.click();
+  } else {
+    button.textContent = String.fromCodePoint(0x1f504);
+  }
 }
 
 function onClick() {
-    window.location.reload();
+  window.location.reload();
 }
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM fully loaded and parsed");
-    onReload()
+window.addEventListener("DOMContentLoaded", () => {
+  // eslint-disable-next-line no-console
+  console.log("DOM fully loaded and parsed");
+  onReload();
 });
