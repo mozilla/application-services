e55b0d35df06315c50a644eef1a3595caa543011
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2025-07-28 17:28:24 -0400

    Suppress some Detekt warnings

--- components/places/android/src/test/java/mozilla/appservices/places/PlacesConnectionTest.kt
+++ components/places/android/src/test/java/mozilla/appservices/places/PlacesConnectionTest.kt
@@ -520,6 +520,7 @@ class PlacesConnectionTest {
     }
 
     @Test
+    @Suppress("LongMethod")
     fun testHistoryMetadataBasics() = runBlocking {
         val currentTime = System.currentTimeMillis()
 
