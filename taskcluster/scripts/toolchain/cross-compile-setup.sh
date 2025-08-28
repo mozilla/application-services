953cf46c36b433625c6c1d2d2931a845581a16a6
bendk <bdk@mozilla.com>
2025-08-28 09:55:08 -0400

    clang-19 fix [ci full] (#6912)
    Fixed the `ln` command arguments.  How did this work before?
    
    - Remove the `-f` flag from `ln`, we want to fail if the paths aren't right.
    - Use `-o` with curl rather than redirection.  This makes the command
      more obvious in the taskcluster output.

--- taskcluster/scripts/toolchain/cross-compile-setup.sh
+++ taskcluster/scripts/toolchain/cross-compile-setup.sh
@@ -27,18 +27,21 @@ export TARGET_CFLAGS="-DNDEBUG"
 # cribbed from glean https://github.com/mozilla/glean/blob/b5faa56305a3a500e49fc02509001c95fde32ee6/taskcluster/scripts/cross-compile-setup.sh
 pushd /builds/worker
 curl -sfSL --retry 5 --retry-delay 10 \
-    https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.cache.level-3.toolchains.v3.linux64-cctools-port.latest/artifacts/public%2Fbuild%2Fcctools.tar.zst > cctools.tar.zst
+    https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.cache.level-3.toolchains.v3.linux64-cctools-port.latest/artifacts/public%2Fbuild%2Fcctools.tar.zst \
+    -o cctools.tar.zst
 unzstd cctools.tar.zst
 tar -xf cctools.tar
 rm cctools.tar.zst
 curl -sfSL --retry 5 --retry-delay 10 \
-    https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.cache.level-3.toolchains.v3.clang-dist-toolchain.latest/artifacts/public%2Fbuild%2Fclang-dist-toolchain.tar.xz > clang-dist-toolchain.tar.xz
+    https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.cache.level-3.toolchains.v3.clang-dist-toolchain.latest/artifacts/public%2Fbuild%2Fclang-dist-toolchain.tar.xz \
+    -o clang-dist-toolchain.tar.xz
 tar -xf clang-dist-toolchain.tar.xz
 mv builds/worker/toolchains/clang clang
 rm clang-dist-toolchain.tar.xz
 
-# Fixup symlink
-ln -sf clang-19 clang/bin/clang
+# The rest of this script expects `clang-19`, but the binary in named `clang` in the tarball.
+# Create a symlink to work around this.
+ln -s /builds/worker/clang/bin/clang clang/bin/clang-19
 
 popd
 
