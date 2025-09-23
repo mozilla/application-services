4df62cb1e824a15a06012051b6a804af2f78b8fc
Ben Dean-Kawamura <bdk@mozilla.com>
2025-09-23 11:45:35 -0400

    Removing old Kotlin viaduct code
    While working on https://bugzilla.mozilla.org/show_bug.cgi?id=1989865, I
    realized we had a bunch of Kotlin code aimed at the old protobuf-based
    viaduct that we don't need anymore.

--- components/viaduct/android/src/main/java/mozilla/appservices/httpconfig/HttpConfig.kt
+++ components/viaduct/android/src/main/java/mozilla/appservices/httpconfig/HttpConfig.kt
@@ -11,9 +11,6 @@ import mozilla.components.concept.fetch.Client
 import mozilla.components.concept.fetch.MutableHeaders
 import mozilla.components.concept.fetch.Request
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.locks.ReentrantReadWriteLock
-import kotlin.concurrent.read
-import kotlin.concurrent.write
 
 /**
  * All errors emitted by the client will subclass this.
@@ -31,12 +28,6 @@ class UnsupportedRequestMethodError(method: String) :
  * used by Rust components.
  */
 object RustHttpConfig {
-    // Protects imp/client
-    private var lock = ReentrantReadWriteLock()
-
-    @Volatile
-    private var client: Lazy<Client>? = null
-
     /**
      * Set the HTTP client to be used by all Rust code.
      * the `Lazy`'s value is not read until the first request is made.
@@ -81,53 +72,6 @@ object RustHttpConfig {
             useCaches = request.useCaches,
         )
     }
-
-    @Suppress("TooGenericExceptionCaught", "ReturnCount")
-    internal fun doFetch(b: RustBuffer.ByValue): RustBuffer.ByValue {
-        lock.read {
-            try {
-                val request = MsgTypes.Request.parseFrom(b.asCodedInputStream())
-                val rb = try {
-                    // Note: `client!!` is fine here, since if client is null,
-                    // we wouldn't have yet initialized
-                    val resp = client!!.value.fetch(convertRequest(request))
-                    val rb = MsgTypes.Response.newBuilder()
-                        .setUrl(resp.url)
-                        .setStatus(resp.status)
-                        .setBody(
-                            resp.body.useStream {
-                                ByteString.readFrom(it)
-                            },
-                        )
-
-                    for (h in resp.headers) {
-                        rb.putHeaders(h.name, h.value)
-                    }
-                    rb
-                } catch (e: Throwable) {
-                    MsgTypes.Response.newBuilder()
-                        .setExceptionMessage("fetch error: ${e.message ?: e.javaClass.canonicalName}")
-                }
-                val built = rb.build()
-                val needed = built.serializedSize
-                val outputBuf = LibViaduct.INSTANCE.viaduct_alloc_bytebuffer(needed)
-                try {
-                    // This is only null if we passed a negative number or something to
-                    // viaduct_alloc_bytebuffer.
-                    val stream = outputBuf.asCodedOutputStream()!!
-                    built.writeTo(stream)
-                    return outputBuf
-                } catch (e: Throwable) {
-                    // Note: we want to clean this up only if we are not returning it to rust.
-                    LibViaduct.INSTANCE.viaduct_destroy_bytebuffer(outputBuf)
-                    LibViaduct.INSTANCE.viaduct_log_error("Failed to write buffer: ${e.message}")
-                    throw e
-                }
-            } finally {
-                LibViaduct.INSTANCE.viaduct_destroy_bytebuffer(b)
-            }
-        }
-    }
 }
 
 internal fun convertMethod(m: MsgTypes.Request.Method): Request.Method {
@@ -141,19 +85,5 @@ internal fun convertMethod(m: MsgTypes.Request.Method): Request.Method {
         MsgTypes.Request.Method.TRACE -> Request.Method.TRACE
         MsgTypes.Request.Method.CONNECT -> Request.Method.CONNECT
         else -> throw UnsupportedRequestMethodError(m.toString())
-    }
-}
-
-internal class CallbackImpl : RawFetchCallback {
-    @Suppress("TooGenericExceptionCaught")
-    override fun invoke(b: RustBuffer.ByValue): RustBuffer.ByValue {
-        try {
-            return RustHttpConfig.doFetch(b)
-        } catch (e: Throwable) {
-            LibViaduct.INSTANCE.viaduct_log_error("doFetch failed: ${e.message}")
-            // This is our last resort. It's bad news should we fail to
-            // return something from this function.
-            return RustBuffer.ByValue()
-        }
     }
 }
