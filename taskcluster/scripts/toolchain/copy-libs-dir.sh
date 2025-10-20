af1ce7008cca78b81d383102ada8c6d5954e6d37
Bastian Gruber <bgruber@mozilla.com>
2025-10-20 09:44:10 -0300

    Update dependency list

--- taskcluster/scripts/toolchain/copy-libs-dir.sh
+++ taskcluster/scripts/toolchain/copy-libs-dir.sh
@@ -2,4 +2,10 @@
 
 set -ex
 
-rsync -av "$(realpath "$MOZ_FETCHES_DIR"/libs)/" "$(realpath "$1")/"
+# Only copy the specific subdirectories we need
+for dir in android desktop ios; do
+    if [ -d "${MOZ_FETCHES_DIR}/libs/${dir}" ]; then
+        mkdir -p "$(realpath "$1")/${dir}"
+        rsync -a "${MOZ_FETCHES_DIR}/libs/${dir}/" "$(realpath "$1")/${dir}/"
+    fi
+done
