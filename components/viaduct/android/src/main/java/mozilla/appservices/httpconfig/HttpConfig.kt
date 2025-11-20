f55d17672d92cd858c37a289caa0ea121242c9ab
bendk <bdk@mozilla.com>
2025-11-20 16:28:49 -0500

    Switch back to the old Android viaduct backend (#7093)
    We've been seeing a large volume of crashes on android
    (https://bugzilla.mozilla.org/show_bug.cgi?id=1992149).  They seem to be
    related to the new viaduct backend.  It's unclear exactly why, but let's
    try going back to the old one and see if it lessens the volume.
    
    This reverts commit 2042b47c960bd079427e8ed827d766c14d6f0e91 so that we
    can use the old backend code.
    
    (cherry picked from commit 9f550793bae296f33da8acc37c86df1e9df2d7e4)

--- components/viaduct/android/src/main/java/mozilla/appservices/httpconfig/HttpConfig.kt
+++ components/viaduct/android/src/main/java/mozilla/appservices/httpconfig/HttpConfig.kt
@@ -10,8 +10,11 @@ import mozilla.components.concept.fetch.Client
 import mozilla.components.concept.fetch.MutableHeaders
 import mozilla.components.concept.fetch.Request
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.concurrent.atomics.AtomicBoolean
 import kotlin.concurrent.atomics.ExperimentalAtomicApi
+import kotlin.concurrent.read
+import kotlin.concurrent.write
 import mozilla.appservices.viaduct.allowAndroidEmulatorLoopback as rustAllowAndroidEmulatorLoopback
 
 /**
@@ -31,9 +34,22 @@ class UnsupportedRequestMethodError(method: String) :
  */
 @OptIn(ExperimentalAtomicApi::class)
 object RustHttpConfig {
-    // Used to only initialize the client once
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1989865.
-    private var backendInitialized = AtomicBoolean(false)
+    // Protects imp/client
+    private var lock = ReentrantReadWriteLock()
+
+    // Add back once we move to the new backend again
+    // // Used to only initialize the client once
+    // // https://bugzilla.mozilla.org/show_bug.cgi?id=1989865.
+    // private var backendInitialized = AtomicBoolean(false)
+
+    @Volatile
+    private var client: Lazy<Client>? = null
+
+    // Important note to future maintainers: if you mess around with
+    // this code, you have to make sure `imp` can't get GCed. Extremely
+    // bad things will happen if it does!
+    @Volatile
+    private var imp: CallbackImpl? = null
 
     /**
      * Set the HTTP client to be used by all Rust code.
@@ -41,8 +57,12 @@ object RustHttpConfig {
      */
     @Synchronized
     fun setClient(c: Lazy<Client>) {
-        if (backendInitialized.compareAndSet(false, true)) {
-            initBackend(FetchBackend(c))
+        lock.write {
+            client = c
+            if (imp == null) {
+                imp = CallbackImpl()
+                LibViaduct.INSTANCE.viaduct_initialize(imp!!)
+            }
         }
     }
 
@@ -53,7 +73,9 @@ object RustHttpConfig {
      * are sure you are running on an emulator.
      */
     fun allowAndroidEmulatorLoopback() {
-        rustAllowAndroidEmulatorLoopback()
+        lock.read {
+            LibViaduct.INSTANCE.viaduct_allow_android_emulator_loopback()
+        }
     }
 
     internal fun convertRequest(request: MsgTypes.Request): Request {
@@ -81,6 +103,53 @@ object RustHttpConfig {
             useCaches = request.useCaches,
         )
     }
+
+    @Suppress("TooGenericExceptionCaught", "ReturnCount")
+    internal fun doFetch(b: RustBuffer.ByValue): RustBuffer.ByValue {
+        lock.read {
+            try {
+                val request = MsgTypes.Request.parseFrom(b.asCodedInputStream())
+                val rb = try {
+                    // Note: `client!!` is fine here, since if client is null,
+                    // we wouldn't have yet initialized
+                    val resp = client!!.value.fetch(convertRequest(request))
+                    val rb = MsgTypes.Response.newBuilder()
+                        .setUrl(resp.url)
+                        .setStatus(resp.status)
+                        .setBody(
+                            resp.body.useStream {
+                                ByteString.readFrom(it)
+                            },
+                        )
+
+                    for (h in resp.headers) {
+                        rb.putHeaders(h.name, h.value)
+                    }
+                    rb
+                } catch (e: Throwable) {
+                    MsgTypes.Response.newBuilder()
+                        .setExceptionMessage("fetch error: ${e.message ?: e.javaClass.canonicalName}")
+                }
+                val built = rb.build()
+                val needed = built.serializedSize
+                val outputBuf = LibViaduct.INSTANCE.viaduct_alloc_bytebuffer(needed)
+                try {
+                    // This is only null if we passed a negative number or something to
+                    // viaduct_alloc_bytebuffer.
+                    val stream = outputBuf.asCodedOutputStream()!!
+                    built.writeTo(stream)
+                    return outputBuf
+                } catch (e: Throwable) {
+                    // Note: we want to clean this up only if we are not returning it to rust.
+                    LibViaduct.INSTANCE.viaduct_destroy_bytebuffer(outputBuf)
+                    LibViaduct.INSTANCE.viaduct_log_error("Failed to write buffer: ${e.message}")
+                    throw e
+                }
+            } finally {
+                LibViaduct.INSTANCE.viaduct_destroy_bytebuffer(b)
+            }
+        }
+    }
 }
 
 internal fun convertMethod(m: MsgTypes.Request.Method): Request.Method {
@@ -94,5 +163,19 @@ internal fun convertMethod(m: MsgTypes.Request.Method): Request.Method {
         MsgTypes.Request.Method.TRACE -> Request.Method.TRACE
         MsgTypes.Request.Method.CONNECT -> Request.Method.CONNECT
         else -> throw UnsupportedRequestMethodError(m.toString())
+    }
+}
+
+internal class CallbackImpl : RawFetchCallback {
+    @Suppress("TooGenericExceptionCaught")
+    override fun invoke(b: RustBuffer.ByValue): RustBuffer.ByValue {
+        try {
+            return RustHttpConfig.doFetch(b)
+        } catch (e: Throwable) {
+            LibViaduct.INSTANCE.viaduct_log_error("doFetch failed: ${e.message}")
+            // This is our last resort. It's bad news should we fail to
+            // return something from this function.
+            return RustBuffer.ByValue()
+        }
     }
 }
