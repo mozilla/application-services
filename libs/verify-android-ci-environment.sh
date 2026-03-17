8c5ffafee14c04a3a91db927672f076ea68a0d80
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2026-03-17 14:49:19 -0400

    Update Kotlin to version 2.3.20

--- libs/verify-android-ci-environment.sh
+++ libs/verify-android-ci-environment.sh
@@ -64,7 +64,7 @@ if [[ ! -d "$ANDROID_HOME/cmdline-tools" ]]; then
   echo "Android SDK is missing command line tools. Install them via Android Studio -> SDK Manager -> SDK Tools."
   exit 1
 fi
-"$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager" "ndk;$(./gradlew -q printNdkVersion | tail -1)"
+#"$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager" "ndk;$(./gradlew -q printNdkVersion | tail -1)"
 
 # CI just downloads these libs anyway.
 if [[ -z "${CI}" ]]; then
