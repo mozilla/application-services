54c93919e888456765b5acffd7472ab9fb66ba6a
Wyatt Verchere <wverchere@gmail.com>
2026-07-20 08:49:59 -0700

    fix: Adds deprecation notes

--- automation/smoke-test-fenix.py
+++ automation/smoke-test-fenix.py
@@ -3,7 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-
+# DEPRECATED. Please consider using ./build_against_fenix.py
 # Purpose: Run Fenix tests against this application-services working tree.
 # Usage: ./automation/smoke-test-fenix.py
 
