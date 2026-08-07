1d969289794a0f91cb403083f295ec9e9843a569
bendk <bdk@mozilla.com>
2026-08-07 10:00:24 -0400

    Fix FxA state machine spelling (#7528)
    A while back we purposely misspelled "Auth" as "Ath" to get around the
    Sentry redaction rules.  Now that we're using the error ping and Grafana
    we don't need this anymore.

--- components/fxa-client/src/state_machine/display.rs
+++ components/fxa-client/src/state_machine/display.rs
@@ -6,9 +6,6 @@
 //!
 //! These are sent to Sentry, so they must not leak PII.
 //! In general this means they don't output values for inner fields.
-//!
-//! Also, they must not use the string "auth" since Sentry will filter that out.
-//! Use "ath" instead.
 
 use super::{FxaEvent, FxaState};
 use std::fmt;
@@ -18,9 +15,9 @@ impl fmt::Display for FxaState {
         let name = match self {
             Self::Uninitialized => "Uninitialized",
             Self::Disconnected => "Disconnected",
-            Self::Authenticating { .. } => "Athenticating",
+            Self::Authenticating { .. } => "Authenticating",
             Self::Connected => "Connected",
-            Self::AuthIssues => "AthIssues",
+            Self::AuthIssues => "AuthIssues",
         };
         write!(f, "{name}")
     }
@@ -30,10 +27,10 @@ impl fmt::Display for FxaEvent {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let name = match self {
             Self::Initialize { .. } => "Initialize",
-            Self::BeginOAuthFlow { .. } => "BeginOAthFlow",
+            Self::BeginOAuthFlow { .. } => "BeginOAuthFlow",
             Self::BeginPairingFlow { .. } => "BeginPairingFlow",
-            Self::CompleteOAuthFlow { .. } => "CompleteOAthFlow",
-            Self::CancelOAuthFlow => "CancelOAthFlow",
+            Self::CompleteOAuthFlow { .. } => "CompleteOAuthFlow",
+            Self::CancelOAuthFlow => "CancelOAuthFlow",
             Self::CheckAuthorizationStatus => "CheckAuthorizationStatus",
             Self::WebChannelPasswordChange { .. } => "WebChannelPwdChange",
             Self::Disconnect => "Disconnect",
