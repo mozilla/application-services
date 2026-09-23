1fe0aa56d9903b26ae764ae32ae225334b2e6625
Ben Dean-Kawamura <bdk@mozilla.com>
2026-09-23 13:54:44 -0400

    Re-enable glean-sym on Android
    Re-enabled the `glean-sym/active` feature, but not when we're building
    in Gradle for Desktop.  Those builds are intended for unit testing.

--- megazords/full/src/lib.rs
+++ megazords/full/src/lib.rs
@@ -76,15 +76,17 @@ unsafe impl Send for StaticCStringPtr {}
 unsafe impl Sync for StaticCStringPtr {}
 
 // The namespace glean-sym FFI items are placed in.
-// These are not defined since we don't set the `active` feature
+// These are not defined when we don't set the `active` feature
 // Add this hack to prevent uniffi_bindgen failures
 
+#[cfg(not(target_os = "android"))]
 #[allow(clippy::large_const_arrays)]
 const UNIFFI_META_CONST_NAMESPACE_GLEAN_SYM: ::uniffi::MetadataBuffer =
     ::uniffi::MetadataBuffer::from_code(::uniffi::metadata::codes::NAMESPACE)
         .concat_str("glean_sym")
         .concat_str("glean_sym");
 
+#[cfg(not(target_os = "android"))]
 #[doc(hidden)]
 #[unsafe(no_mangle)]
 pub static UNIFFI_META_NAMESPACE_GLEAN_SYM: [::std::primitive::u8;
